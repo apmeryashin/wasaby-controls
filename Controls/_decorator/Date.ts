011005deab637bb7ed4663545d99ca11a2b1c7cb
mk.siryakov <mk.siryakov@tensor.ru>
2021-07-23 11:12:49 +0300

    https://online.sbis.ru/opendoc.html?guid=101f288b-6e35-4420-be5d-7e3e360ea900

--- Controls/_decorator/Date.ts
+++ Controls/_decorator/Date.ts
@@ -25,7 +25,7 @@ export interface IDateOptions extends IControlOptions, IFontColorStyleOptions, I
     /**
      * @name Controls/_decorator/IDate#format
      * @cfg {string} Формат для преобразования даты.
-     * Все доступные маски описаны {@link https://platform-git.sbis.ru/saby/types/-/blob/2eedb730647fadf517d65afa5f2588f9322e2858/Types/_formatter/date.ts#L445 здесь}
+     * Все доступные маски описаны {@link https://wi.sbis.ru/docs/js/Types/formatter/methods/date/ здесь}
      * @demo Controls-demo/Decorator/Date/Index
      */
     format: string;
