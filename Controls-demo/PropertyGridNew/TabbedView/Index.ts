386f587e2c9d024cb3214b66226178e2e1fe948d
ea.melnikova <ea.melnikova@tensor.ru>
2021-07-29 09:51:55 +0300

    https://online.sbis.ru/opendoc.html?guid=5fe98943-917f-4549-84f8-7bdfd087afa5

--- Controls-demo/PropertyGridNew/TabbedView/Index.ts
+++ Controls-demo/PropertyGridNew/TabbedView/Index.ts
@@ -24,7 +24,7 @@ export default class TabbedViewDemo extends Control {
         count: 750
     };
 
-    protected _typeDescription: unknown[] = [
+    protected _source: unknown[] = [
         {
             name: 'description',
             caption: 'Описание',
