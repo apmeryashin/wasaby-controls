d236ff00d2156f07383788a5e3a77e871882cf19
am.gerasimov <am.gerasimov@tensor.ru>
2021-07-29 14:11:07 +0300

    https://online.sbis.ru/opendoc.html?guid=cf9fedeb-2279-4ae9-a3c4-99fff189bf39

---
+++ Controls/Utils/IsLetterPressedOnKeyboard.ts
@@ -0,0 +1,29 @@
+import { SyntheticEvent } from 'UICommon/Events';
+/**
+ * Модуль экспортирующий функцию, проверяющую что на клавиатуре была нажата символьная клавиша
+ * @class Controls/Utils/isLetterPressedOnKeyboard
+ * @public
+ * @author Герасимов А.М
+ */
+
+function getActiveElement(): HTMLElement|void {
+    return document && document.activeElement as HTMLElement;
+}
+/**
+ * Возвращает признак, что на клавиатуре была нажата символьная клавиша
+ * @function Controls/Utils/isLetterPressedOnKeyboard#isLetterPressedOnKeyboard
+ * @param event {Event} дескриптор события
+ * @returns Boolean
+ */
+
+export default function isLetterPressedOnKeyboard(event: SyntheticEvent|KeyboardEvent): boolean {
+    const nativeEvent = event.nativeEvent || event;
+    const isSpecialKeyPressed = nativeEvent.altKey || nativeEvent.ctrlKey || nativeEvent.shiftKey;
+    const isLetterPressed = /^([a-zA-Z]|[а-яА-Я]|[0-9])$/.test(nativeEvent.key);
+    const activeElement = getActiveElement();
+    const allowProcessKeydown = activeElement &&
+        activeElement.tagName !== 'INPUT' &&
+        activeElement.tagName !== 'TEXTAREA';
+
+    return !isSpecialKeyPressed && isLetterPressed && allowProcessKeydown && nativeEvent.type === 'keydown';
+}
