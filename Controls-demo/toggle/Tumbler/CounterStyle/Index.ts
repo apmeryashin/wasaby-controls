fb06e482321c5ea678bf8d951aadfdeb3e2f7157
ma.mochalov <ma.mochalov@tensor.ru>
2021-07-30 09:55:00 +0300

    https://online.sbis.ru/opendoc.html?guid=455f542f-3965-4219-a780-bb0d6dc9b806

--- Controls-demo/toggle/Tumbler/CounterStyle/Index.ts
+++ Controls-demo/toggle/Tumbler/CounterStyle/Index.ts
@@ -2,71 +2,38 @@ import {Control, TemplateFunction} from 'UI/Base';
 import * as Template from "wml!Controls-demo/toggle/Tumbler/CounterStyle/CounterStyle";
 import {RecordSet} from 'Types/collection';
 
+interface CounterStyleItem {
+    style: string;
+    items: RecordSet;
+}
+
 export default class extends Control {
     protected _template: TemplateFunction = Template;
-    protected _items: RecordSet;
+    protected _items: CounterStyleItem[] = [];
     protected _selectedKey: string = '1';
 
     protected _beforeMount(): void {
-        this._items = new RecordSet({
-            rawData: [
-                {
-                    id: '1',
-                    caption: 'secondary',
-                    counter: 15,
-                    counterStyle: 'secondary'
-                },
-                {
-                    id: '2',
-                    caption: 'primary',
-                    counter: 15,
-                    counterStyle: 'primary'
-                },
-                {
-                    id: '3',
-                    caption: 'warning',
-                    counter: 15,
-                    counterStyle: 'warning'
-                },
-                {
-                    id: '4',
-                    caption: 'danger',
-                    counter: 15,
-                    counterStyle: 'danger'
-                },
-                {
-                    id: '5',
-                    caption: 'unaccented',
-                    counter: 15,
-                    counterStyle: 'unaccented'
-                },
-                {
-                    id: '6',
-                    caption: 'link',
-                    counter: 15,
-                    counterStyle: 'link'
-                },
-                {
-                    id: '7',
-                    caption: 'label',
-                    counter: 15,
-                    counterStyle: 'label'
-                },
-                {
-                    id: '8',
-                    caption: 'info',
-                    counter: 15,
-                    counterStyle: 'info'
-                },
-                {
-                    id: '9',
-                    caption: 'default',
-                    counter: 15,
-                    counterStyle: 'default'
-                }
-            ],
-            keyProperty: 'id'
+        const styles = ['secondary', 'primary', 'warning', 'danger', 'unaccented', 'link', 'label', 'info', 'default']
+        styles.forEach(style=>{
+            this._items.push({
+                style,
+                items: new RecordSet({
+                    rawData: [
+                    {
+                        id: '1',
+                        caption: 'Название 1',
+                        count: 10,
+                        counter: 15,
+                        counterStyle: style
+                    },
+                    {
+                        id: '2',
+                        caption: 'Название 2'
+                    }
+                ]})
+            });
         });
     }
+
     static _styles: string[] = ['Controls-demo/Controls-demo'];
 }
