2e619f9a4adbe001d3f606fa129205e5dbb1924c
ia.klepikov <ia.klepikov@tensor.ru>
2021-07-30 15:34:58 +0300

    https://online.sbis.ru/opendoc.html?guid=deb994ff-c9c8-4c8b-bf72-ee16ea0aa888

---
+++ Controls/_themes/Wrapper.ts
@@ -0,0 +1,44 @@
+import {Control, TemplateFunction, IControlOptions} from 'UI/Base';
+import * as template from 'wml!Controls/_themes/Wrapper/Wrapper';
+
+export interface IWrapperOptions extends IControlOptions {
+    prefix: string;
+    variables: Record<string, string>;
+}
+
+/**
+ * ХОК для установки css-переменных в inline-стили.
+ * @class Controls/_themes/Wrapper
+ * @extends UI/Base:Control
+ * @author Клепиков И.А.
+ * @public
+ */
+export default class Wrapper <TWrapperOptions extends IWrapperOptions> extends Control<TWrapperOptions> {
+    protected _template: TemplateFunction = template;
+    protected _themeVariables: string;
+
+    protected _beforeMount(options?: TWrapperOptions): void {
+        this._themeVariables =
+            Wrapper.prepareStyleValue(this.computeStylesLogic(options), options.prefix);
+    }
+
+    protected _beforeUpdate(options: TWrapperOptions): void {
+        if (JSON.stringify(options.variables) !== JSON.stringify(this._options.variables)) {
+            this._themeVariables =
+                Wrapper.prepareStyleValue(this.computeStylesLogic(options), options.prefix);
+        }
+    }
+
+    protected computeStylesLogic(options: TWrapperOptions): Record<string, string> {
+        return options.variables;
+    }
+
+    static prepareStyleValue(variablesObj: Record<string, string>, prefix: string): string {
+        let result = '';
+        const _prefix = prefix ? prefix + '_' : '';
+        Object.keys(variablesObj).forEach((key) => {
+            result += ['--', _prefix, key, ':', variablesObj[key], ';'].join('');
+        });
+        return result;
+    }
+}
