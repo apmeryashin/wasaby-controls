1c94c91738a8e807d6a60e11e35885af9b9f80c0
in.kondratev <in.kondratev@tensor.ru>
2021-08-02 13:47:40 +0300

    https://online.sbis.ru/opendoc.html?guid=d8938c24-9d0e-4f0a-9d80-a0707bb6bd51

--- Controls-demo/Popup/Page/templates/ScrollableWidget.ts
+++ Controls-demo/Popup/Page/templates/ScrollableWidget.ts
@@ -1,6 +1,5 @@
 import {Control, TemplateFunction, IControlOptions} from 'UI/Base';
 import * as Template from 'wml!Controls-demo/Popup/Page/templates/ScrollableWidget';
-import 'css!Controls-demo/Popup/Page/templates/ScrollableWidget';
 
 interface IStackOptions extends IControlOptions {
     prefetchResult: Record<string, any>;
