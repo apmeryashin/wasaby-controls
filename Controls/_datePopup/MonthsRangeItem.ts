544622a920c8ffcc6774c9a15784f444809c8d3d
gd.kovalev <gd.kovalev@tensor.ru>
2021-08-04 09:59:57 +0700

    https://online.sbis.ru/opendoc.html?guid=c7a6cb2a-081b-4abb-aac4-862b9f4ba517

--- Controls/_datePopup/MonthsRangeItem.ts
+++ Controls/_datePopup/MonthsRangeItem.ts
@@ -189,6 +189,7 @@ export default class MonthsRangeItem extends Control<IMonthsRangeItemOptions> {
         const itemClass = '.controls-PeriodDialog-MonthsRange__item';
         const mode = 'months';
         this._notify('itemKeyDown', [item, event.nativeEvent.keyCode, itemClass, mode]);
+        // Останавливаем нативный скролл, когда управляем выбором через стрелочки клавиатуры
         event.preventDefault();
     }
 
