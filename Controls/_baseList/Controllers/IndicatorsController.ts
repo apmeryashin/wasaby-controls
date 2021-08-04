50b04f809173ac7ac84bd9abe8297ea213c81f5b
as.avramenko <as.avramenko@tensor.ru>
2021-08-04 09:31:22 +0300

    https://online.sbis.ru/opendoc.html?guid=40eb7d4a-448c-4581-89d4-d17fc3f8d204

--- Controls/_baseList/Controllers/IndicatorsController.ts
+++ Controls/_baseList/Controllers/IndicatorsController.ts
@@ -375,7 +375,7 @@ export default class IndicatorsController {
         // Делаем это именно тут, чтобы попасть в единый цикл отрисовки с коллекцией.
         // Пересчёт после отрисовки с пустой коллекцией не подходит, т.к. уже словим событие скрытия триггера.
         // https://online.sbis.ru/opendoc.html?guid=2754d625-f6eb-469d-9fb5-3c86e88e793e
-        const hasItems = this._model && !!this._model.getCount();
+        const hasItems = this._model && !this._model.destroyed && !!this._model.getCount();
         if (!hasItems) {
             this.setLoadingTriggerOffset({
                 top: 0,
