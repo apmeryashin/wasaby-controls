4950d605c8522864c5cdf44e6ef171473496ba50
in.kondratev <in.kondratev@tensor.ru>
2021-08-06 15:56:08 +0300

    https://online.sbis.ru/opendoc.html?guid=dcc29ae0-5712-48fa-9339-72fb22a6e2cb

---
+++ Controls-demo/Decorator/Money/Hovered/Index.ts
@@ -0,0 +1,20 @@
+import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
+import template = require('wml!Controls-demo/Decorator/Money/Hovered/Template');
+import 'css!Controls/CommonClasses';
+import 'css!Controls-demo/Controls-demo';
+
+class Hovered extends Control<IControlOptions> {
+    protected _template: TemplateFunction = template;
+    protected _hoveredStateForDoubleColor: boolean = false;
+    protected _hoveredStateForTreeColor: boolean = false;
+
+    protected _mouseEventHandlerForDoubleColor(): void {
+        this._hoveredStateForDoubleColor = !this._hoveredStateForDoubleColor;
+    }
+
+    protected _mouseEventHandlerForTreeColor(): void {
+        this._hoveredStateForTreeColor = !this._hoveredStateForTreeColor;
+    }
+}
+
+export default Hovered;
