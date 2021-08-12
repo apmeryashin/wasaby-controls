8dd0d4ade5c0a4b27dfc322b0fcf268efd9dc92a
va.kolesov <va.kolesov@tensor.ru>
2021-08-12 11:08:57 +0300

    https://online.sbis.ru/opendoc.html?guid=242cc04f-62e0-4696-bd64-95c28cf38d7d
    Демо

--- Controls-demo/tileNew/DifferentItemTemplates/MediumTemplate/RoundBorder/s/Index.ts
+++ Controls-demo/tileNew/DifferentItemTemplates/MediumTemplate/RoundBorder/s/Index.ts
@@ -1,25 +1,22 @@
 import {Control, TemplateFunction} from 'UI/Base';
 import {HierarchicalMemory} from 'Types/source';
-import {IItemAction} from 'Controls/itemActions';
 
-import * as Template from 'wml!Controls-demo/tileNew/DifferentItemTemplates/RichTemplate/RoundBorder/xs/xs';
+import * as Template from 'wml!Controls-demo/tileNew/DifferentItemTemplates/MediumTemplate/RoundBorder/s/s';
 
-import {DATA, ITEM_ACTIONS} from 'Controls-demo/tileNew/DataHelpers/RichTemplate';
+import {Gadgets} from '../../../../DataHelpers/DataCatalog';
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
     protected _viewSource: HierarchicalMemory = null;
     protected _selectedKeys: string[] = [];
-    protected _itemActions: IItemAction[];
-    protected _roundBorder = {'tl': 'xs', 'tr': 'xs', 'br': 'xs', 'bl': 'xs'};
+    protected _roundBorder = {'tl': 's', 'tr': 's', 'br': 's', 'bl': 's'};
 
     protected _beforeMount(): void {
         this._viewSource = new HierarchicalMemory({
-            keyProperty: 'id',
+            keyProperty: 'key',
             parentProperty: 'parent',
-            data: DATA
+            data: Gadgets.getData()
         });
-        this._itemActions = ITEM_ACTIONS;
     }
 
     static _styles: string[] = ['Controls-demo/Controls-demo'];
