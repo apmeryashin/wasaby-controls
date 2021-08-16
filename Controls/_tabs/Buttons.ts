280adb3840dd5a543fb463befeb4afeae8d61474
ma.mochalov <ma.mochalov@tensor.ru>
2021-08-16 13:46:52 +0300

    https://online.sbis.ru/opendoc.html?guid=3b8734cd-412f-4303-897f-e73fe55e477d

--- Controls/_tabs/Buttons.ts
+++ Controls/_tabs/Buttons.ts
@@ -9,7 +9,7 @@ import {SyntheticEvent} from 'Vdom/Vdom';
 import {isLeftMouseButton} from 'Controls/popup';
 import {IItems, IItemTemplateOptions} from 'Controls/interface';
 import {ITabsButtons, ITabsButtonsOptions, ITabButtonItem} from './interface/ITabsButtons';
-import {constants} from 'Env/Env';
+import {constants, detection} from 'Env/Env';
 import {adapter} from 'Types/entity';
 import {factory} from 'Types/chain';
 import Marker from './Buttons/Marker';
@@ -425,6 +425,17 @@ class TabsButtons extends Control<ITabsOptions, IReceivedState> implements ITabs
         const itemType: string = item.type || 'default';
         return `controls-Tabs__itemClickableArea_type-${itemType}`;
     }
+
+
+    protected _prepareItemMinWidthClass(item: ITabButtonItem): string {
+        if (detection.isIE &&
+            (!this._getTemplate(this._options.itemLeftTemplate, item, this._options.leftTemplateProperty) &&
+                !this._getTemplate(this._options.itemLeftTemplate, item, this._options.rightTemplateProperty))) {
+            return 'controls-Tabs__itemClickableArea_minWidth';
+        }
+        return '';
+    }
+
 
     protected _prepareItemMarkerClass(item: ITabButtonItem): string {
         const classes = [];
