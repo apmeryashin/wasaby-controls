d1f095d6ba381f6b23651c5c082da79e370d6d9e
ma.mochalov <ma.mochalov@tensor.ru>
2021-08-18 10:53:27 +0300

    https://online.sbis.ru/opendoc.html?guid=5b3159cb-bbd1-46b0-ad86-2ec8cd41d560

--- Controls/_toolbars/View.ts
+++ Controls/_toolbars/View.ts
@@ -185,7 +185,6 @@ class Toolbar extends Control<IToolbarOptions, TItems> implements IHierarchy, II
     protected _parentProperty: string = null;
     protected _isLoadMenuItems: boolean = false;
     protected _firstItem: TItem = null;
-    protected _countShowItems: number = 0;
     protected _buttonTemplate: TemplateFunction = getButtonTemplate();
     private _menuItems: {
         [key: number]: TItems
@@ -393,12 +392,6 @@ class Toolbar extends Control<IToolbarOptions, TItems> implements IHierarchy, II
 
     private _setStateByItems(items: TItems, source?: ICrudPlus): void {
         this._items = items;
-        this._countShowItems = 0;
-        items.forEach((item) => {
-            if (this._isShowToolbar(item as TItem, this._parentProperty)) {
-                this._countShowItems++;
-            }
-        });
         // у первой записи тулбара не требуется показывать отступ слева
         this._firstItem = this._getFirstToolbarItem() as TItem;
         if (source) {
