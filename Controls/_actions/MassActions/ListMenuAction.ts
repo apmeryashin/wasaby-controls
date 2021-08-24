bc207670eb7680a9ba104851cf78288137771904
se.mihaylov <se.mihaylov@tensor.ru>
2021-08-24 09:14:37 +0300

    https://online.sbis.ru/opendoc.html?guid=f408c93d-c1b7-489f-bc59-79bf9ac470b1

--- Controls/_actions/MassActions/ListMenuAction.ts
+++ Controls/_actions/MassActions/ListMenuAction.ts
@@ -5,7 +5,10 @@ import {DataSet} from 'Types/source';
 export default abstract class ListMenuAction extends MassAction {
     constructor(options) {
         super(options);
-        this['parent@'] = true;
     }
     abstract load(root: string | number): Promise<RecordSet | DataSet>;
 }
+
+Object.assign(ListMenuAction.prototype, {
+    'parent@': true
+});
