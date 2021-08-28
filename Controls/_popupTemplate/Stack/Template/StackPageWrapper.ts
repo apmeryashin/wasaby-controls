1f740dd4e93e1597373e110fa795c2521671dfc0
Andrew <kr.andrew94@mail.ru>
2021-08-28 22:38:28 +0300

    https://online.sbis.ru/opendoc.html?guid=d2a520f8-0b71-4ea3-8dd3-7302e0471896 https://online.sbis.ru/opendoc.html?guid=ed939aba-0fa6-45f8-b401-bc9455ef8ce6

--- Controls/_popupTemplate/Stack/Template/StackPageWrapper.ts
+++ Controls/_popupTemplate/Stack/Template/StackPageWrapper.ts
@@ -3,6 +3,8 @@ import * as template from 'wml!Controls/_popupTemplate/Stack/Template/StackPageW
 import {getPopupWidth, savePopupWidth, IStackSavedConfig} from 'Controls/_popupTemplate/Util/PopupWidthSettings';
 import {RIGHT_PANEL_WIDTH} from 'Controls/_popupTemplate/BaseController';
 import {IPropStorage, IPropStorageOptions} from 'Controls/interface';
+import {RegisterClass} from 'Controls/event';
+import {SyntheticEvent} from 'Vdom/Vdom';
 
 interface IPageTemplate extends IControlOptions, IPropStorageOptions {
     minWidth: number;
@@ -34,12 +36,15 @@ export default class StackPageWrapper extends Control<IPageTemplate, IReceivedSt
     protected _maxOffset: number;
     protected _canResize: boolean;
     protected _minWidth: number;
+    private _resizeRegister: RegisterClass;
+    private _offsetChanged: boolean;
 
     protected _beforeMount(options?: IPageTemplate, context?: object,
                            receivedState?: IReceivedState): void | Promise<IReceivedState> {
         this._setWorkSpaceWidth(receivedState?.width || options.workspaceWidth);
         this._updateOffset(options);
         this._updateProperties(options);
+        this._resizeRegister = new RegisterClass({register: 'controlResize'});
         if (!receivedState && options.propStorageId) {
             return new Promise((resolve) => {
                 getPopupWidth(options.propStorageId).then((data?: number | IStackSavedConfig) => {
@@ -58,6 +63,22 @@ export default class StackPageWrapper extends Control<IPageTemplate, IReceivedSt
         }
     }
 
+    protected _afterRender(): void {
+       if (this._offsetChanged) {
+           this._resizeRegister.start(new SyntheticEvent({}));
+       }
+    }
+
+    protected _registerHandler(event: Event, registerType: string,
+                               component: Control, callback: Function, config): void {
+        this._resizeRegister.register(event, registerType, component, callback, config);
+    }
+
+    protected _unregisterHandler(event: Event, registerType: string,
+                                 component: Control, config): void {
+        this._resizeRegister.unregister(event, registerType, component, config);
+    }
+
     protected _offsetHandler(event: Event, offset: number): void {
         const newWidth = this._workspaceWidth + offset;
         this._setWorkSpaceWidth(newWidth);
@@ -68,7 +89,7 @@ export default class StackPageWrapper extends Control<IPageTemplate, IReceivedSt
         };
         savePopupWidth(this._options.propStorageId, data);
         this._updateOffset();
-
+        this._offsetChanged = true;
         // Так же как в реестрах, сообщаем про смену размеров рабочей области.
         this._notify('workspaceResize', [this._workspaceWidth], {bubbling: true});
     }
