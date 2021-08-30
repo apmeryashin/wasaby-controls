aff5c0dcd9796ad8f57cac13c76354d0555239a9
Gersasha <42995382+Gersasha@users.noreply.github.com>
2021-08-30 15:49:25 +0300

    Update Search.ts

--- Controls-demo/Filter_new/Search/Search.ts
+++ Controls-demo/Filter_new/Search/Search.ts
@@ -6,40 +6,37 @@ import {Memory} from 'Types/source';
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
-    protected _filter: object = {};
     protected _filterSource: object[] = null;
     protected _source: Memory = null;
-    protected _searchValue: string = '';
 
     protected _beforeMount(): void {
         this._filterSource = [{
             name: 'owner',
             value: '0',
             resetValue: '0',
+            filterTemplate: 'Controls/filterPopup:Dropdown',
             editorOptions: {
                 source: new Memory({
                     keyProperty: 'lastName',
                     data: [
-                        { id: 0, owner: 'По ответственному', lastName: '0' },
+                        { id: 'Yaroslavl', cityName: 'Yaroslavl' },
                         { id: 1, owner: 'Новиков Д.В.', lastName: 'Новиков Д.В.' },
                         { id: 2, owner: 'Кошелев А.Е.', lastName: 'Кошелев А.Е.' },
                         { id: 3, owner: 'Субботин А.В.', lastName: 'Субботин А.В.' },
                         { id: 4, owner: 'Чеперегин А.С.', lastName: 'Чеперегин А.С.' }
                     ]
                 }),
-                displayProperty: 'owner',
-                keyProperty: 'lastName'
+                displayProperty: 'cityName',
+                keyProperty: 'id'
             },
             viewMode: 'frequent'
         }];
 
         this._source = new Memory({
             keyProperty: 'id',
-            data: departments,
-            filter: memorySourceFilter({
-                owner: '0',
-                department: 'По департаменту'
-            })
+            data: [{
+                
+            }]
         });
     }
     static _styles: string[] = ['Controls-demo/Controls-demo', 'Controls-demo/Filter_new/Filter'];
