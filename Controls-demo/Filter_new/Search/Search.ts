5798eb91e101ad45df60079c9bcc79f19e669769
ea.melnikova <ea.melnikova@tensor.ru>
2021-08-30 14:46:31 +0300

    https://online.sbis.ru/opendoc.html?guid=31408153-5cdb-478a-8e63-4fb9cf9110e3

---
+++ Controls-demo/Filter_new/Search/Search.ts
@@ -0,0 +1,46 @@
+import {Control, TemplateFunction} from 'UI/Base';
+import * as Template from 'wml!Controls-demo/Filter_new/Search/Search';
+import {departments} from 'Controls-demo/Filter_new/resources/DataStorage';
+import memorySourceFilter = require('Controls-demo/Utils/MemorySourceFilter');
+import {Memory} from 'Types/source';
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+    protected _filter: object = {};
+    protected _filterSource: object[] = null;
+    protected _source: Memory = null;
+    protected _searchValue: string = '';
+
+    protected _beforeMount(): void {
+        this._filterSource = [{
+            name: 'owner',
+            value: '0',
+            resetValue: '0',
+            editorOptions: {
+                source: new Memory({
+                    keyProperty: 'lastName',
+                    data: [
+                        { id: 0, owner: 'По ответственному', lastName: '0' },
+                        { id: 1, owner: 'Новиков Д.В.', lastName: 'Новиков Д.В.' },
+                        { id: 2, owner: 'Кошелев А.Е.', lastName: 'Кошелев А.Е.' },
+                        { id: 3, owner: 'Субботин А.В.', lastName: 'Субботин А.В.' },
+                        { id: 4, owner: 'Чеперегин А.С.', lastName: 'Чеперегин А.С.' }
+                    ]
+                }),
+                displayProperty: 'owner',
+                keyProperty: 'lastName'
+            },
+            viewMode: 'frequent'
+        }];
+
+        this._source = new Memory({
+            keyProperty: 'id',
+            data: departments,
+            filter: memorySourceFilter({
+                owner: '0',
+                department: 'По департаменту'
+            })
+        });
+    }
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
