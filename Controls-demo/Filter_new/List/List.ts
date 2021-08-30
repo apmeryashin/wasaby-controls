a8cc466b1569dfb0f85cf8dcf884ebb034769cf3
ea.melnikova <ea.melnikova@tensor.ru>
2021-08-30 17:57:15 +0300

    https://online.sbis.ru/opendoc.html?guid=dd80015f-2253-4e05-bc93-47a66e8f1241

--- Controls-demo/Filter_new/List/List.ts
+++ Controls-demo/Filter_new/List/List.ts
@@ -1,44 +1,47 @@
 import {Control, TemplateFunction} from 'UI/Base';
 import * as Template from 'wml!Controls-demo/Filter_new/List/List';
-import {departments} from 'Controls-demo/Filter_new/resources/DataStorage';
 import memorySourceFilter = require('Controls-demo/Utils/MemorySourceFilter');
 import {Memory} from 'Types/source';
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
-    protected _filter: object = {};
     protected _filterSource: object[] = null;
     protected _source: Memory = null;
 
     protected _beforeMount(): void {
         this._filterSource = [{
-            name: 'owner',
-            value: '0',
-            resetValue: '0',
+            name: 'city',
+            value: null,
+            resetValue: null,
+            emptyText: 'Choose city',
+            emptyKey: 10,
+            filterTemplate: 'Controls/filterPopup:Dropdown',
             editorOptions: {
                 source: new Memory({
-                    keyProperty: 'lastName',
+                    keyProperty: 'id',
                     data: [
-                        { id: 0, owner: 'По ответственному', lastName: '0' },
-                        { id: 1, owner: 'Новиков Д.В.', lastName: 'Новиков Д.В.' },
-                        { id: 2, owner: 'Кошелев А.Е.', lastName: 'Кошелев А.Е.' },
-                        { id: 3, owner: 'Субботин А.В.', lastName: 'Субботин А.В.' },
-                        { id: 4, owner: 'Чеперегин А.С.', lastName: 'Чеперегин А.С.' }
+                        { id: 'Yaroslavl', city: 'Yaroslavl' },
+                        { id: 'Moscow', city: 'Moscow' },
+                        { id: 'Kazan', city: 'Kazan' }
                     ]
                 }),
-                displayProperty: 'owner',
-                keyProperty: 'lastName'
+                displayProperty: 'city',
+                keyProperty: 'id'
             },
             viewMode: 'frequent'
         }];
 
         this._source = new Memory({
             keyProperty: 'id',
-            data: departments,
+            data: [
+                {id: '1', title: 'Yaroslavl city', city: 'Yaroslavl'},
+                {id: '2', title: 'Moscow city', city: 'Moscow'},
+                {id: '3', title: 'Kazan city', city: 'Kazan'}
+            ],
             filter: memorySourceFilter({
-                owner: '0'
+                city: null
             })
         });
     }
-    static _styles: string[] = ['Controls-demo/Controls-demo', 'Controls-demo/Filter_new/Filter'];
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
 }
