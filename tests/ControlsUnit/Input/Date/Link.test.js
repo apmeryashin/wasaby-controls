5a305278a108ef0b14f791b0921dea9f723084cd
gd.kovalev <gd.kovalev@tensor.ru>
2021-08-31 17:17:01 +0700

    https://online.sbis.ru/opendoc.html?guid=f876b654-2465-4522-9611-50df534c2308

--- tests/ControlsUnit/Input/Date/Link.test.js
+++ tests/ControlsUnit/Input/Date/Link.test.js
@@ -20,16 +20,11 @@ define([
       describe('openPopup', function() {
          it('should open opener', function() {
             const component = calendarTestUtils.createComponent(dateRange.DateSelector, options);
-            component._children = {
-               opener: {
-                  open: sinon.fake()
-               },
-               linkView: {
-                   getPopupTarget: sinon.stub().returns()
-               }
+            component._stickyOpener = {
+               open: sinon.fake()
             };
             component.openPopup();
-            sinon.assert.called(component._children.opener.open);
+            sinon.assert.called(component._stickyOpener.open);
          });
       });
 
@@ -40,7 +35,7 @@ define([
                component = calendarTestUtils.createComponent(dateRange.DateSelector, options),
                value = new Date(2018, 11, 10);
 
-            component._children.opener = {
+            component._stickyOpener = {
                close: sinon.fake()
             };
             sandbox.stub(component, '_notify');
@@ -48,7 +43,7 @@ define([
             component._onResult(null, value);
 
             sinon.assert.calledWith(component._notify, 'valueChanged');
-            sinon.assert.called(component._children.opener.close);
+            sinon.assert.called(component._stickyOpener.close);
             sandbox.restore();
          });
       });
@@ -59,7 +54,7 @@ define([
                component = calendarTestUtils.createComponent(dateRange.DateSelector, options),
                value = new Date(2018, 11, 10);
 
-            component._children.opener = {
+            component._stickyOpener = {
                close: sinon.fake()
             };
             sandbox.stub(component, '_notify');
