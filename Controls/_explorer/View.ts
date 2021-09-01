c61b05510288784ac84be92a2899877a3a156a0e
ka.panihin <ka.panihin@tensor.ru>
2021-09-01 10:26:36 +0300

    https://online.sbis.ru/opendoc.html?guid=88d6cd46-a905-4ca0-82f8-258dcd391e6a

--- Controls/_explorer/View.ts
+++ Controls/_explorer/View.ts
@@ -1151,7 +1151,8 @@ export default class Explorer extends Control<IExplorerOptions> {
             // проставится setViewMode, т.к. он может проставится асинхронно
             // а код ниже вызывает изменение версии модели что приводит к лишней
             // перерисовке до изменения viewMode
-            if (cfg.searchNavigationMode !== 'expand') {
+            const isAllExpanded = cfg.expandedItems && cfg.expandedItems[0] === null;
+            if (cfg.searchNavigationMode !== 'expand' && !isAllExpanded) {
                 this._children.treeControl.resetExpandedItems();
             }
         });
