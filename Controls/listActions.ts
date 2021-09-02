6af0a6f6099a68850ce1cf4a66a962ee8a4e376d
ea.melnikova <ea.melnikova@tensor.ru>
2021-09-02 10:49:41 +0300

    https://online.sbis.ru/opendoc.html?guid=a470b071-eca0-4c5a-b821-999be2e46074

--- Controls/listActions.ts
+++ Controls/listActions.ts
@@ -4,12 +4,17 @@
  * @includes IAction Controls/_listCommands/interface/IAction
  */
 
-export {default as IAction, default as ICommand} from './_listCommands/interface/IAction';
-export {default as IActionOptions, default as ICommandOptions} from './_listCommands/interface/IActionOptions';
-export {default as Remove} from './_listCommands/Remove';
-export {default as RemoveProvider} from './_listCommands/Remove/Provider';
-export {default as RemoveProviderWithConfirm} from './_listCommands/Remove/ProviderWithConfirm';
-export {default as Move, IMoveActionOptions} from './_listCommands/Move';
-export {default as MoveProviderWithDialog} from './_listCommands/Move/ProviderWithDialog';
-export {default as MoveProvider} from './_listCommands/Move/Provider';
-export {default as MoveProviderDirection} from './_listCommands/Move/ProviderDirection';
+export {
+    IAction,
+    ICommand,
+    IActionOptions,
+    ICommandOptions,
+    Remove,
+    RemoveProvider,
+    RemoveProviderWithConfirm,
+    Move,
+    IMoveActionOptions,
+    MoveProviderWithDialog,
+    MoveProvider,
+    MoveProviderDirection
+} from 'Controls/listCommands';
