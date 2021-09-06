9433f7fdc51c7f48f91db3bba134ae49e4206773
yan.sirotkin <yan.sirotkin@tensor.ru>
2021-09-06 13:26:38 +0300

    https://online.sbis.ru/opendoc.html?guid=55d02a28-f218-4586-ba0f-f4866e4663f2 fixed typos

--- Controls/_tile/interface/IRichTemplate.ts
+++ Controls/_tile/interface/IRichTemplate.ts
@@ -20,7 +20,7 @@ interface ICharacteristicsItem {
  * "Богатый" шаблон отображения элементов в {@link Controls/tile:View плитке}.
  * @class Controls/_tile/interface/IRichTemplate
  * @implements Controls/tile:ItemTemplate
- * @author Михайлов С.Е
+ * @author Колесов В.А
  * @see Controls/tile:View
  * @example
  * <pre class="brush: html; highlight: [3-11]">
