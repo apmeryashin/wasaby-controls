01855e77d3c646eab5fa996952aa2fdafb1ce5eb
pa.averkiev <pa.averkiev@tensor.ru>
2021-09-15 14:34:03 +0300

    https://online.sbis.ru/opendoc.html?guid=92a2fb71-b9da-4248-91c9-cb6337722c33

---
+++ Controls-demo/explorerNew/ItemActions/OutsideWithBreadCrumbs/Index.ts
@@ -0,0 +1,44 @@
+import {Control, TemplateFunction} from 'UI/Base';
+import {Gadgets} from 'Controls-demo/explorerNew/DataHelpers/DataCatalog';
+import { IColumn } from 'Controls/grid';
+import {TRoot} from 'Controls-demo/types';
+import { IHeaderCell } from 'Controls/grid';
+import {HierarchicalMemory} from 'Types/source';
+import {IItemAction} from 'Controls/itemActions';
+
+import * as Template from 'wml!Controls-demo/explorerNew/ItemActions/OutsideWithBreadCrumbs/OutsideWithBreadCrumbs';
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+    protected _viewSource: HierarchicalMemory;
+    protected _columns: IColumn[] = Gadgets.getGridColumns();
+    protected _viewMode: string = 'table';
+    protected _root: TRoot = 1;
+    protected _header: IHeaderCell[] = Gadgets.getHeader();
+    protected _itemActions: IItemAction[];
+
+    protected _beforeMount(): void {
+        this._viewSource = new HierarchicalMemory({
+            keyProperty: 'id',
+            parentProperty: 'parent',
+            data: Gadgets.getData()
+        });
+
+        this._itemActions = [
+            {
+                id: 1,
+                icon: 'icon-PhoneNull',
+                title: 'phone',
+                showType: 2
+            },
+            {
+                id: 2,
+                icon: 'icon-EmptyMessage',
+                title: 'message',
+                showType: 2
+            }
+        ];
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
