f7dbf1c748a3cbb2a653175f33fa53fa7c1acc40
ka.panihin <ka.panihin@tensor.ru>
2021-09-22 08:49:20 +0300

    https://online.sbis.ru/opendoc.html?guid=2e3b1374-6b8a-4e5a-bf89-1cc29c1f497d

--- Controls/_baseList/BaseControl.ts
+++ Controls/_baseList/BaseControl.ts
@@ -1366,7 +1366,7 @@ const _private = {
 
                         self._indicatorsController.onCollectionReset();
 
-                        if (self._options.searchValue) {
+                        if (_private.isPortionedLoad(self)) {
                             // Событие reset коллекции приводит к остановке активного порционного поиска.
                             // В дальнейшем (по необходимости) он будет перезапущен в нужных входных точках.
                             self._indicatorsController.endDisplayPortionedSearch();
@@ -3807,11 +3807,6 @@ export default class BaseControl<TOptions extends IBaseControlOptions = IBaseCon
         // region Indicators
 
         this._updateIndicatorsController(newOptions, isSourceControllerLoadingNow);
-
-        // При смене searchValue завeршаем порционный поиск и потом в нужном месте его запустим(reset или loadToDirection)
-        if (this._options.searchValue !== newOptions.searchValue) {
-            this._indicatorsController.endDisplayPortionedSearch();
-        }
 
         if (loadStarted && !this._indicatorsController.hasDisplayedIndicator()) {
             this._displayGlobalIndicator();
