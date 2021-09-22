45eb4d6b3b9e8c4db06dd3ed126b971cf56f0117
as.avramenko <as.avramenko@tensor.ru>
2021-09-22 12:16:32 +0300

    Неправильное событие использовалось в beforeUpdate, в итоге событие ресайза списка вообще не стреляло https://online.sbis.ru/opendoc.html?guid=546c35a1-0d51-4372-ae47-7dd9aa4de951

--- Controls-demo/gridNew/ColumnScroll/ScrollStartPositionEnd/Index.ts
+++ Controls-demo/gridNew/ColumnScroll/ScrollStartPositionEnd/Index.ts
@@ -1,4 +1,5 @@
 import {Control, TemplateFunction} from 'UI/Base';
+import { RecordSet } from 'Types/collection';
 import * as Template from 'wml!Controls-demo/gridNew/ColumnScroll/ScrollStartPositionEnd/ScrollStartPositionEnd';
 import {Memory} from 'Types/source';
 import { IColumn } from 'Controls/grid';
@@ -8,6 +9,7 @@ import { Countries } from 'Controls-demo/gridNew/DemoHelpers/Data/Countries';
 export default class extends Control {
     protected _template: TemplateFunction = Template;
     protected _viewSource: Memory;
+    protected _items: RecordSet;
     protected _columns: IColumn[] = Countries.getColumnsWithWidths();
     protected _header: IHeaderCell[] = Countries.getHeader();
 
@@ -16,6 +18,24 @@ export default class extends Control {
             keyProperty: 'key',
             data: Countries.getData()
         });
+        this._itemsReadyCallback = this._itemsReadyCallback.bind(this);
+    }
+
+    protected _itemsReadyCallback(items: RecordSet): void {
+        this._items = items;
+    }
+    protected _onClickAdd(): void {
+        const newItem = this._items.at(0).clone();
+        newItem.set({
+            key: 999,
+            number: 999,
+            country: 'Южная Африканская Республика',
+            capital: 'Претория, Кейптаун, Блумфонтейн',
+            population: 54956900,
+            square: 1219912,
+            populationDensity: 41
+        });
+        this._items.add(newItem);
     }
 
     static _styles: string[] = ['Controls-demo/Controls-demo'];
