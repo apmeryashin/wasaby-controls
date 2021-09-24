25c15ed706b083cf0daf045d134a897e38d5d807
ea.melnikova <ea.melnikova@tensor.ru>
2021-09-24 13:45:24 +0300

    https://online.sbis.ru/opendoc.html?guid=4a437b27-fcae-47e2-9a09-d7fb7fb60da4

---
+++ Controls-demo/filterPanel/CompositeFilter/Index.ts
@@ -0,0 +1,70 @@
+import {Control, TemplateFunction} from 'UI/Base';
+import * as Template from 'wml!Controls-demo/filterPanel/CompositeFilter/Index';
+import {Memory} from 'Types/source';
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+    protected _filterButtonSource: object[] = [];
+    protected _source: Memory = null;
+    protected _gridColumns: object[] = [
+        {
+            displayProperty: 'title'
+        }, {
+            displayProperty: 'amount'
+        }
+    ];
+
+    protected _beforeMount(): void {
+        const data = [
+            {department: 'Разработка', title: 'Разработка', amount: 10},
+            {department: 'Продвижение СБИС', title: 'Продвижение СБИС', amount: 3},
+            {department: 'Федеральная клиентская служка', title: 'Федеральная клиентская служка', amount: 30}
+        ];
+        this._source = new Memory({
+            data,
+            keyProperty: 'department',
+            filter: (item, queryFilter) => {
+                let addToData = true;
+                for (const filterField in queryFilter) {
+                    if (queryFilter.hasOwnProperty(filterField) && item.get(filterField) && addToData) {
+                        const filterValue = queryFilter[filterField];
+                        const itemValue = item.get(filterField);
+                        const itemValueIsNumber = typeof itemValue === 'number';
+                        addToData = ((itemValue >= filterValue[0] || !filterValue[0]) &&
+                            (itemValue <= filterValue[1] || !filterValue[1])) && itemValueIsNumber || (filterValue.includes(itemValue) || !filterValue.length);
+                    }
+                }
+                return addToData;
+            }
+        });
+        this._filterButtonSource = [
+            {
+                group: 'Количество сотрудников',
+                name: 'amount',
+                editorTemplateName: 'Controls/filterPanel:NumberRangeEditor',
+                resetValue: [],
+                caption: '',
+                value: [],
+                textValue: ''
+            }, {
+                group: 'Отдел',
+                name: 'department',
+                resetValue: [],
+                caption: '',
+                value: [],
+                textValue: '',
+                editorTemplateName: 'Controls/filterPanel:ListEditor',
+                editorOptions: {
+                    keyProperty: 'department',
+                    displayProperty: 'title',
+                    source: new Memory({
+                        data,
+                        keyProperty: 'department'
+                    })
+                }
+            }
+        ];
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo', 'Controls-demo/filterPanel/Index'];
+}
