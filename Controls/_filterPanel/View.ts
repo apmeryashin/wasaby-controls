51e33f6ada18e585972d100174666a8b1e906fdd
ea.melnikova <ea.melnikova@tensor.ru>
2021-09-28 12:48:19 +0300

    https://online.sbis.ru/opendoc.html?guid=75948421-2db9-42f5-9116-af881b269e95

--- Controls/_filterPanel/View.ts
+++ Controls/_filterPanel/View.ts
@@ -125,6 +125,7 @@ export default class View extends Control<IViewPanelOptions> {
         if (isResetClick) {
             this._resetFilterItem(dispItem);
         }
+        //Будет удалено после: https://online.sbis.ru/opendoc.html?guid=53839728-e9dc-4af0-88a8-e847b5b4c5f8
         if (isResultClick) {
             this._notify('groupClick');
         }
