8e29ac88185112ff07f775f7f9e7ddbfd23a5b53
pa.averkiev <pa.averkiev@tensor.ru>
2021-09-29 15:44:59 +0300

    https://online.sbis.ru/opendoc.html?guid=d75b3acf-a0ec-4528-a993-d3a1f142ef9c

---
+++ Controls-demo/treeGridNew/Offsets/WithoutRootExpander/Index.ts
@@ -0,0 +1,42 @@
+import {Control, TemplateFunction} from 'UI/Base';
+import {CrudEntityKey, HierarchicalMemory} from 'Types/source';
+import {Model} from 'Types/entity';
+import {IColumn} from 'Controls/grid';
+import {Flat} from 'Controls-demo/treeGridNew/DemoHelpers/Data/Flat';
+
+import * as Template from 'wml!Controls-demo/treeGridNew/Offsets/WithoutRootExpander/WithoutRootExpander';
+
+export default class extends Control {
+   protected _template: TemplateFunction = Template;
+   protected _viewSource: HierarchicalMemory;
+   protected _expandedItems: CrudEntityKey[] = [null];
+   protected _columns: IColumn[];
+
+   protected _beforeMount(): void {
+      this._columns = [
+         {
+            displayProperty: 'title'
+         }
+      ];
+      this._viewSource = new HierarchicalMemory({
+         keyProperty: 'key',
+         data: Flat.getData(),
+         filter: () => true,
+         parentProperty: 'parent'
+      });
+   }
+
+   /**
+    * Принудительно скрываем иконку expander для узлов первого уровня
+    * @param item
+    * @private
+    */
+   protected _getExpanderIcon(item: Model): string {
+      if (item.get('parent') === null) {
+         return 'none';
+      }
+      return;
+   }
+
+   static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
