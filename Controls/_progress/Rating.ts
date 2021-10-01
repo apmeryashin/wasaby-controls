6d70a25b52b89008261c24a8a7ffa93a4474dd0c
kraynovdo <do.kraynov@tensor.ru>
2021-10-01 11:57:51 +0300

    https://online.sbis.ru/opendoc.html?guid=94062f36-ad65-4573-811f-a1a0e6add93c

--- Controls/_progress/Rating.ts
+++ Controls/_progress/Rating.ts
@@ -10,185 +10,22 @@ type IconSize = 'default'|'2xs'|'xs'|'s'|'m'|'l';
 type IconStyle = 'warning'|'info'|'success'|'danger'|'secondary'|'primary'|'default'|'contrast'|'readonly';
 type IconPadding = 'null'|'3xs'|'2xs'|'xs'|'s'|'m'|'l'|'xl';
 type TPrecision = 0 | 0.5;
+type TIconFill = 'none' | 'full';
 
 const DEFAULT_ICON_SIZE = 's';
 const DEFAULT_ICON_PADDING = '3xs';
 const DEFAULT_ICON_STYLE = 'warning';
 const DEFAULT_EMPTY_ICON_STYLE = 'readonly';
 
-/**
- * Интерфейс опций для {@link Controls/progress:Rating}.
- * @interface Controls/progress:IRating
- * @public
- * @author Колесов В.А.
- */
 interface IRatingOptions extends IControlOptions {
-    /**
-     * @name Controls/progress:IRating#value
-     * @cfg {Number} Количество заполненных звезд
-     * @remark
-     * Число от 1 до 5. Допускается ввод дробных чисел. Если десятичное значение value больше половины целого значения, и в precision установленно 0.5, то показывается пол.звезды.
-     * @see Controls/progress:IRating#precision
-     */
-    /*
-     * @name Controls/progress:IRating#value
-     * @cfg {Number} Number of highlighted stars
-     * @remark
-     * An float from 1 to 5.
-     */
     value: number;
-    /**
-     * @name Controls/progress:IRating#precision
-     * @cfg {Number} Точность рейтинга
-     * @variable 0 - отображение полностью закрашенных звезд
-     * @variable 0.5 - отображение закрашенных на половину звезд
-     * @default 0
-     * @remark
-     * Если десятичное значение value больше половины целого значения, и в precision установленно 0.5, то показывается пол.звезды.
-     * 3,44 – 3 звезды. 3,56 – 3 с половиной здезды
-     */
-    /*
-     * @name Controls/progress:IRating#precision
-     * @cfg {Number} precision rating
-     * @variable 0 - displays fully filled stars
-     * @variable 0.5 - display half-filled stars
-     * @ default 0
-     * @remark
-     * If the decimal "value" of value is more than half an integer value, and exactly 0.5 is set, then a half star is shown.
-     * 3,44 – 3 highlighted stars. 3,56 – 3 with half highlighted stars
-     */
     precision?: TPrecision;
-    /**
-     * @name Controls/progress:IRating#readOnly
-     * @cfg {Boolean} Определяет, может ли пользователь изменить значение контрола.
-     * @remark
-     * Если значение false, то при наведении на пустую звездуона и все предыдущие до нее заполняются, при уводе фокуса
-     * становятся обратно пустыми. При клике на какую-либо звезду устанавливается рейтинг слева на право.
-     */
-    /*
-     * @name Controls/progress:IRating#readOnly
-     * @cfg {Boolean} Determines if the user can change the value of the control.
-     * @remark
-     * If the value is false, then when you hover over an empty star, that and all the previous ones before it are filled,
-     * when the focus is removed, they become empty back. When you click on any star, the rating is set from left to right.
-     */
     readOnly?: boolean;
-    /**
-     * @name Controls/progress:IRating#iconSize
-     * @cfg {String} Размер иконки звезды.
-     * @variant default
-     * @variant 2xs
-     * @variant xs
-     * @variant s
-     * @variant m
-     * @variant l
-     * @see iconPadding
-     * @see iconStyle
-     * @see emptyIconStyle
-     */
-    /*
-     * @name Controls/progress:IRating#iconSize
-     * @cfg {String} Star size
-     * @remark
-     * Possible values:
-     * * default
-     * * 2xs
-     * * xs
-     * * s
-     * * m
-     * * l
-     */
     iconSize?: IconSize;
-    /**
-     * @name Controls/progress:IRating#iconStyle
-     * @cfg {String} Цвет заполненной звезды.
-     * @variant warning
-     * @variant info
-     * @variant success
-     * @variant danger
-     * @variant secondary
-     * @variant primary
-     * @variant default
-     * @variant contrast
-     * @see iconSize
-     * @see iconPadding
-     * @see emptyIconStyle
-     */
-    /*
-     * @name Controls/progress:IRating#iconStyle
-     * @cfg {String} Color of highlighted star
-     * @remark
-     * Possible values:
-     * * warning
-     * * info
-     * * success
-     * * danger
-     * * secondary
-     * * primary
-     * * default
-     * * contrast
-     */
     iconStyle?: IconStyle;
-    /**
-     * @name Controls/progress:IRating#iconPadding
-     * @cfg {String} Расстояние между звездами.
-     * @variant null
-     * @variant 2xs
-     * @variant xs
-     * @variant s
-     * @variant m
-     * @variant l
-     * @variant xl
-     * @see iconSize
-     * @see iconStyle
-     * @see emptyIconStyle
-     */
-    /*
-     * @name Controls/progress:IRating#iconPadding
-     * @cfg {Number} Distance between stars
-     * @remark
-     * Possible values:
-     * * null
-     * * 2xs
-     * * xs
-     * * s
-     * * m
-     * * l
-     * * xl
-     */
     iconPadding?: IconPadding;
-    /**
-     * @name Controls/progress:IRating#emptyIconStyle
-     * @cfg {String} Цвет пустой звезды.
-     * @variant warning
-     * @variant info
-     * @variant success
-     * @variant danger
-     * @variant secondary
-     * @variant primary
-     * @variant default
-     * @variant contrast
-     * @variant readonly
-     * @see iconPadding
-     * @see iconStyle
-     * @see iconSize
-     */
-    /*
-     * @name Controls/progress:IRating#emptyIconStyle
-     * @cfg {String} Color of empty star
-     * @remark
-     * Possible values:
-     * * warning
-     * * info
-     * * success
-     * * danger
-     * * secondary
-     * * primary
-     * * default
-     * * contrast
-     * * readonly
-     */
     emptyIconStyle?: IconStyle;
+    emptyIconFill?: TIconFill;
 }
 
 /**
@@ -197,19 +34,20 @@ interface IRatingOptions extends IControlOptions {
  * @remark
  * Полезные ссылки:
  * * {@link /materials/Controls-demo/app/Controls-demo%2Fprogress%2FRating%2FIndex демо-пример}
+ * @class Controls/progress:Rating
  * @extends UI/Base:Control
  * @implements Controls/progress:IRating
  * @author Нигматуллина Л.Э.
  * @public
  *
- * @demo Controls-demo/progress/Rating/Index
+ * @demo Controls-demo/progress/Rating/Base/Index
  */
 
 /*
  * Control of rating
  * Render highlighted stars depending on the rating
+ * @class Controls/progress:Rating
  * @extends UI/Base:Control
- * @implements Controls/progress:IRating
  * @author Nigmatullina L.E.
  * @public
  *
@@ -226,7 +64,8 @@ class Rating extends Control<IRatingOptions> {
             value: options.value,
             precision: options.precision,
             iconStyle: options.iconStyle,
-            emptyIconStyle: options.emptyIconStyle
+            emptyIconStyle: options.emptyIconStyle,
+            emptyIconFill: options.emptyIconFill
         });
     }
 
@@ -242,7 +81,8 @@ class Rating extends Control<IRatingOptions> {
                 value: options.value,
                 precision: options.precision,
                 iconStyle: options.iconStyle,
-                emptyIconStyle: options.emptyIconStyle
+                emptyIconStyle: options.emptyIconStyle,
+                emptyIconFill: options.emptyIconFill
             });
         }
     }
@@ -295,4 +135,167 @@ Object.defineProperty(Rating, 'defaultProps', {
     }
 });
 
+/**
+ * @name Controls/progress:Rating#value
+ * @cfg {Number} Количество заполненных звезд
+ * @demo Controls-demo/progress/Rating/Base/Index
+ * @remark
+ * Число от 1 до 5. Допускается ввод дробных чисел. Если десятичное значение value больше половины целого значения, и в precision установленно 0.5, то показывается пол.звезды.
+ * @see Controls/progress:Rating#precision
+ */
+/*
+ * @name Controls/progress:Rating#value
+ * @cfg {Number} Number of highlighted stars
+ * @remark
+ * An float from 1 to 5.
+ */
+/**
+ * @name Controls/progress:Rating#precision
+ * @cfg {Number} Точность рейтинга
+ * @variable 0 - отображение полностью закрашенных звезд
+ * @variable 0.5 - отображение закрашенных на половину звезд
+ * @default 0
+ * @demo Controls-demo/progress/Rating/Base/Index
+ * @remark
+ * Если десятичное значение value больше половины целого значения, и в precision установленно 0.5, то показывается пол.звезды.
+ * 3,44 – 3 звезды. 3,56 – 3 с половиной здезды
+ */
+/*
+ * @name Controls/progress:Rating#precision
+ * @cfg {Number} precision rating
+ * @variable 0 - displays fully filled stars
+ * @variable 0.5 - display half-filled stars
+ * @ default 0
+ * @remark
+ * If the decimal "value" of value is more than half an integer value, and exactly 0.5 is set, then a half star is shown.
+ * 3,44 – 3 highlighted stars. 3,56 – 3 with half highlighted stars
+ */
+
+/**
+ * @name Controls/progress:Rating#iconSize
+ * @cfg {String} Размер иконки звезды.
+ * @variant default
+ * @variant 2xs
+ * @variant xs
+ * @variant s
+ * @variant m
+ * @variant l
+ * @see iconPadding
+ * @see iconStyle
+ * @see emptyIconStyle
+ * @demo Controls-demo/progress/Rating/IconSize/Index
+ */
+/*
+ * @name Controls/progress:Rating#iconSize
+ * @cfg {String} Star size
+ * @remark
+ * Possible values:
+ * * default
+ * * 2xs
+ * * xs
+ * * s
+ * * m
+ * * l
+ */
+/**
+ * @name Controls/progress:Rating#iconStyle
+ * @cfg {String} Цвет заполненной звезды.
+ * @variant warning
+ * @variant info
+ * @variant success
+ * @variant danger
+ * @variant secondary
+ * @variant primary
+ * @variant default
+ * @variant contrast
+ * @see iconSize
+ * @see iconPadding
+ * @see emptyIconStyle
+ * @demo Controls-demo/progress/Rating/IconStyle/Index
+ */
+/*
+ * @name Controls/progress:Rating#iconStyle
+ * @cfg {String} Color of highlighted star
+ * @remark
+ * Possible values:
+ * * warning
+ * * info
+ * * success
+ * * danger
+ * * secondary
+ * * primary
+ * * default
+ * * contrast
+ */
+/**
+ * @name Controls/progress:Rating#iconPadding
+ * @cfg {String} Расстояние между звездами.
+ * @variant null
+ * @variant 2xs
+ * @variant xs
+ * @variant s
+ * @variant m
+ * @variant l
+ * @variant xl
+ * @see iconSize
+ * @see iconStyle
+ * @see emptyIconStyle
+ * @demo Controls-demo/progress/Rating/IconPadding/Index
+ */
+/*
+ * @name Controls/progress:Rating#iconPadding
+ * @cfg {Number} Distance between stars
+ * @remark
+ * Possible values:
+ * * null
+ * * 2xs
+ * * xs
+ * * s
+ * * m
+ * * l
+ * * xl
+ */
+/**
+ * @name Controls/progress:Rating#emptyIconStyle
+ * @cfg {String} Цвет пустой звезды.
+ * @variant warning
+ * @variant info
+ * @variant success
+ * @variant danger
+ * @variant secondary
+ * @variant primary
+ * @variant default
+ * @variant contrast
+ * @variant readonly
+ * @see iconPadding
+ * @see iconStyle
+ * @see iconSize
+ * @demo Controls-demo/progress/Rating/EmptyIconStyle/Index
+ */
+/*
+ * @name Controls/progress:Rating#emptyIconStyle
+ * @cfg {String} Color of empty star
+ * @remark
+ * Possible values:
+ * * warning
+ * * info
+ * * success
+ * * danger
+ * * secondary
+ * * primary
+ * * default
+ * * contrast
+ * * readonly
+ */
+/**
+ * @name Controls/progress:Rating#emptyIconFill
+ * @cfg {String} Заливка пустой звезды
+ * @variant none Заливки нет, есть только контур
+ * @variant full Заливка есть
+ * @see iconPadding
+ * @see iconStyle
+ * @see iconSize
+ * @demo Controls-demo/progress/Rating/EmptyIconFill/Index
+ * @remark Обратите внимание, если опция установлена в значение full, то значения опций iconStyle и emptyIconStyle должны различаться
+ */
 export default Rating;
