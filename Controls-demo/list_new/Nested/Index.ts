cd3365a13862050c069e069a9ae4a1c4726f69fb
pa.averkiev <pa.averkiev@tensor.ru>
2021-10-11 17:48:32 +0300

    https://online.sbis.ru/opendoc.html?guid=1bcdb528-74bd-4117-bbb6-1c521a28d3c8

---
+++ Controls-demo/list_new/Nested/Index.ts
@@ -0,0 +1,54 @@
+import {Control, TemplateFunction, IControlOptions} from 'UI/Base';
+import {Memory} from 'Types/source';
+
+import * as Template from 'wml!Controls-demo/list_new/Nested/Nested';
+
+const rootData = [
+    {
+        key: 1,
+        group: 'Группа верхнего уровня',
+        title: 'Запись списка верхнего уровня. В шаблоне содержится вложенный список'
+    }
+];
+
+const nestedData = [
+    {
+        key: 1,
+        group: 'Группа #1 вложенного списка',
+        title: 'Запись #1 вложенного списка'
+    },
+    {
+        key: 2,
+        group: 'Группа #1 вложенного списка',
+        title: 'Запись #2 вложенного списка'
+    },
+    {
+        key: 3,
+        group: 'Группа #2 вложенного списка',
+        title: 'Запись #3 вложенного списка'
+    },
+    {
+        key: 4,
+        group: 'Группа #2 вложенного списка',
+        title: 'Запись #4 вложенного списка'
+    }
+];
+
+export default class extends Control<IControlOptions> {
+    protected _template: TemplateFunction = Template;
+    protected _rootSource: Memory;
+    protected _nestedSource: Memory;
+
+    protected _beforeMount(options?: IControlOptions, contexts?: object, receivedState?: void): Promise<void> | void {
+        this._rootSource = new Memory({
+            keyProperty: 'key',
+            data: rootData
+        });
+        this._nestedSource = new Memory({
+            keyProperty: 'key',
+            data: nestedData
+        });
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
