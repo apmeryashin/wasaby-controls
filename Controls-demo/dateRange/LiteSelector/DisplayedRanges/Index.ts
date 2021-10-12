0a974ca3de8bfb8cb1fb6119b11f3b915a7c03d8
gd.kovalev <gd.kovalev@tensor.ru>
2021-10-12 11:13:06 +0700

    https://online.sbis.ru/opendoc.html?guid=3c62cd02-92d2-4843-9dc2-560545589b0f

--- Controls-demo/dateRange/LiteSelector/DisplayedRanges/Index.ts
+++ Controls-demo/dateRange/LiteSelector/DisplayedRanges/Index.ts
@@ -5,7 +5,7 @@ class DemoControl extends Control<IControlOptions> {
     protected _template: TemplateFunction = template;
 
     protected _displayedRanges = [[new Date(2018, 0), new Date(2022, 0)], [new Date(2025, 0), new Date(2030, 0)]];
-    protected _displayedRanges1 = [[new Date(2020, 0), new Date(2021, 1)]];
+    protected _displayedRanges1 = [[new Date(2020, 0), new Date(2021, 0)]];
     protected _displayedRanges3 = [[new Date(2018, 0), new Date(2022, 0)], [new Date(2025, 0), new Date(2045, 0)]];
     protected _startValue1: Date = new Date(2019, 1);
     protected _endValue1: Date = new Date(2019, 2, 0);
