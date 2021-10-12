9752d9d5a900cc5f5ca37205b4246b58212b66cc
ma.mochalov <ma.mochalov@tensor.ru>
2021-10-12 12:27:51 +0300

    https://online.sbis.ru/opendoc.html?guid=a6dd8e95-ca13-4b05-993b-23d077001ce9

--- Controls/tabs.ts
+++ Controls/tabs.ts
@@ -19,16 +19,13 @@ export {default as AdaptiveButtons, ITabsAdaptiveButtonsOptions} from 'Controls/
 export {ITabsButtons, ITabsButtonsOptions} from 'Controls/_tabs/interface/ITabsButtons';
 export {IIconCounterTabTemplate} from 'Controls/_tabs/interface/IIconCounterTabTemplate';
 export {ITextCounterTabTemplate} from 'Controls/_tabs/interface/ITextCounterTabTemplate';
-export {ITextTabTemplate} from 'Controls/_tabs/interface/ITextTabTemplate';
 
 import buttonsItemTemplate = require('wml!Controls/_tabs/Buttons/ItemTemplate');
 import TextCounterTabTemplate = require('wml!Controls/_tabs/Buttons/templates/TextCounterTabTemplate');
-import TextTabTemplate = require('wml!Controls/_tabs/Buttons/templates/TextTabTemplate');
 import IconCounterTabTemplate = require('wml!Controls/_tabs/Buttons/templates/IconCounterTabTemplate');
 
 export {
    buttonsItemTemplate,
    TextCounterTabTemplate,
-   IconCounterTabTemplate,
-   TextTabTemplate
+   IconCounterTabTemplate
 };
