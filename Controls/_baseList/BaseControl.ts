f980359c3a56ecc7084f57cf1a02564e4a992dac
am.gerasimov <am.gerasimov@tensor.ru>
2021-10-13 11:36:57 +0300

    https://online.sbis.ru/opendoc.html?guid=657b119d-905e-4dd9-b631-d5d8510de747
    # Conflicts:
    #	Controls/_baseList/interface/IList.ts
    #	Controls/_propertyGrid/PropertyGrid.ts

--- Controls/_baseList/BaseControl.ts
+++ Controls/_baseList/BaseControl.ts
@@ -3001,6 +3001,7 @@ const _private = {
  * @implements Controls/list:IEditableList
  * @mixes Controls/_list/BaseControl/Styles
  * @implements Controls/list:IList
+ * @implements Controls/interface:IItemPadding
  * @implements Controls/itemActions:IItemActions
  * @implements Controls/interface:ISorting
  * @implements Controls/list:IMovableList
