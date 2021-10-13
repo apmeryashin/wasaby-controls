8f1b411a1eb21c3d50823872975c0f747b1cde72
pa.averkiev <pa.averkiev@tensor.ru>
2021-10-13 12:50:05 +0300

    https://online.sbis.ru/opendoc.html?guid=60df64c9-1adc-4462-9e52-6e777820c345

--- Controls/_baseList/interface/IList.ts
+++ Controls/_baseList/interface/IList.ts
@@ -213,7 +213,7 @@ export interface IList extends IItemActionsOptions, IMarkerListOptions, IMovable
 /**
  * @name Controls/_list/interface/IList#multiSelectTemplate
  * @cfg {TemplateFunction|String} Пользовательский шаблон множественного выбора.
- * @demo Controls-demo/list_new/Multiselect/CircleTemplate/Index
+ * @demo Controls-demo/list_new/MultiSelect/CircleTemplate/Index
  * @description
  * Доступны следующие платформенные шаблоны множественного выбора:
  * 1. Controls/list:MultiSelectTemplate - множественный выбор при помощи чекбоксов (по умолчанию)
