07f3aa7f6635f180afb7dac4df16816fde37211e
pa.averkiev <pa.averkiev@tensor.ru>
2021-10-14 14:12:04 +0300

    https://online.sbis.ru/opendoc.html?guid=6323d4c2-d6bf-4b3f-bca7-e87548343d08

--- Controls/_treeGrid/display/TreeGridGroupDataCell.ts
+++ Controls/_treeGrid/display/TreeGridGroupDataCell.ts
@@ -63,7 +63,7 @@ export default class TreeGridGroupDataCell<T extends Model = Model> extends mixi
         classes += this._getContentAlignClasses();
         classes += ' controls-ListView__groupContent';
 
-        classes += 'controls-ListView__groupContent_height';
+        classes += ' controls-ListView__groupContent_height';
         if (this.shouldDisplayItemActions()) {
             classes += '_withItemActions';
         }
