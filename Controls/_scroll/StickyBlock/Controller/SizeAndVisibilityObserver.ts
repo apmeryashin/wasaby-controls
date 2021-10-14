50c211cbbd7bd2c41192d67e51b1295883464c24
sa.pyankov <sa.pyankov@tensor.ru>
2021-10-14 09:45:56 +0700

    Update SizeAndVisibilityObserver.ts
    https://online.sbis.ru/opendoc.html?guid=df5065ef-abb0-452b-bf3f-998bd88b5f58

--- Controls/_scroll/StickyBlock/Controller/SizeAndVisibilityObserver.ts
+++ Controls/_scroll/StickyBlock/Controller/SizeAndVisibilityObserver.ts
@@ -70,7 +70,7 @@ export default class SizeAndVisibilityObserver {
     }
 
     private _isHeaderOfGroup(id: number): boolean {
-        return !this._headers[id];
+        return this._headers[id] !== undefined && !this._headers[id];
     }
 
     private _isGroup(id: string): boolean {
