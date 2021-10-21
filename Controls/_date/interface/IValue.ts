30f063085c73215d7de77835c37001bab5f4849f
gd.kovalev <gd.kovalev@tensor.ru>
2021-10-21 16:25:36 +0700

    https://online.sbis.ru/opendoc.html?guid=86244501-198e-40e7-a427-5bf171d29786

---
+++ Controls/_date/interface/IValue.ts
@@ -0,0 +1,25 @@
+export default interface IValueOptions {
+    value: Date | null;
+}
+
+/**
+ * Интерфейс для контролов, которые поддерживают возможность ввода и выбора даты.
+ *
+ * @interface Controls/_date/interface/IValue
+ * @public
+ * @author Красильников А.С.
+ */
+
+/**
+ * @name Controls/_date/interface/IValue#value
+ * @cfg {Date} Выбранная дата
+ * @example
+ * WML:
+ * <pre>
+ *  <Controls.date:Selector value="{{ _value }}" />
+ * </pre>
+ * JS:
+ * <pre>
+ *  _value: Date = new Date(2021, 0);
+ * </pre>
+ */
