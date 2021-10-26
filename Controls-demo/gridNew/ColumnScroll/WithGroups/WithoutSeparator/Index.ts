f3072594865aa73b2941a346d49e2f481f805cf2
pa.averkiev <pa.averkiev@tensor.ru>
2021-10-26 11:54:54 +0300

    https://online.sbis.ru/opendoc.html?guid=491fdc2e-8413-4da0-9a54-1a48b6411f14

---
+++ Controls-demo/gridNew/ColumnScroll/WithGroups/WithoutSeparator/Index.ts
@@ -0,0 +1,38 @@
+import {Control, TemplateFunction} from 'UI/Base';
+import {Memory} from 'Types/source';
+import { IColumn } from 'Controls/grid';
+
+import { Tasks } from 'Controls-demo/gridNew/DemoHelpers/Data/Tasks';
+
+import * as Template from 'wml!Controls-demo/gridNew/ColumnScroll/WithGroups/WithoutSeparator/WithoutSeparator';
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+    protected _viewSource: Memory;
+    protected _separatorVisibility: boolean = false;
+    protected _columns: IColumn[] = [
+        ...Tasks.getDefaultColumns(),
+        {
+            displayProperty: 'message',
+            width: '150px'
+        },
+        {
+            displayProperty: 'fullName',
+            width: '150px'
+        }
+    ];
+    protected _header = this._columns.map((c) => ({caption: c.displayProperty}));
+
+    protected _beforeMount(): void {
+        this._viewSource = new Memory({
+            keyProperty: 'key',
+            data: Tasks.getData()
+        });
+    }
+
+    protected _onToggleSeparatorVisibility(): void {
+        this._separatorVisibility = !this._separatorVisibility;
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
