28c11ceca8f05293f0bd6eda02f4731c40fee68c
ka.panihin <ka.panihin@tensor.ru>
2021-10-27 13:29:23 +0300

    https://online.sbis.ru/opendoc.html?guid=a51061e8-7362-46cc-a705-04eab40e02f2

--- Controls/_baseList/BaseControl.ts
+++ Controls/_baseList/BaseControl.ts
@@ -697,7 +697,7 @@ const _private = {
                 // После выполнения поиска мы должны поставить маркер.
                 // Если выполняется порционный поиск и первый запрос не вернул ни одной записи,
                 // то на событие reset список будет пустой и нам некуда будет ставить маркер.
-                if (_private.hasMarkerController(self) && self._indicatorsController.isDisplayedPortionedSearch()) {
+                if (_private.hasMarkerController(self) && _private.isPortionedLoad(self)) {
                     const newMarkedKey = _private.getMarkerController(self).onCollectionReset();
                     self._changeMarkedKey(newMarkedKey);
                 }
