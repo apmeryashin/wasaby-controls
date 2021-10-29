c8b9daa782670465404a7bb778258f14a291c1b4
ea.rodionov1 <ea.rodionov1@tensor.ru>
2021-10-29 16:51:06 +0300

    https://online.sbis.ru/opendoc.html?guid=77b91347-dbaf-426c-a085-c688d202a3d3

---
+++ tests/ControlsUnit/treeGrid_clean/Display/NodeFooterCell.test.ts
@@ -0,0 +1,36 @@
+import {CssClassesAssert} from 'ControlsUnit/CustomAsserts';
+import {TreeGridNodeFooterCell} from 'Controls/treeGrid';
+
+describe('Controls/treeGrid_clean/Display/NodeFooterCell', () => {
+    let columnIndex = 0;
+    const getMockedOwner = () => ({
+        getColumnIndex: () => columnIndex,
+        hasMultiSelectColumn: () => false,
+        isFullGridSupport: () => true
+    });
+
+    describe('.getWrapperClasses()', () => {
+        beforeEach(() => {
+            columnIndex = 0;
+        });
+        it('first cell without separators', () => {
+            columnIndex = 0;
+            const cell = new TreeGridNodeFooterCell({
+                owner: getMockedOwner(),
+                columnSeparatorSize: 's'
+            });
+
+            CssClassesAssert.notInclude(cell.getWrapperClasses(), 'controls-Grid__columnSeparator_size-s');
+        });
+
+        it('second cell with separators', () => {
+            columnIndex = 1;
+            const cell = new TreeGridNodeFooterCell({
+                owner: getMockedOwner(),
+                columnSeparatorSize: 's'
+            });
+
+            CssClassesAssert.include(cell.getWrapperClasses(), 'controls-Grid__columnSeparator_size-s');
+        });
+    });
+});
