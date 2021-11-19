e9d1eebd93d1260db4229c81587053c9e5c1e0c9
ee.zolotova <ee.zolotova@tensor.ru>
2021-11-19 14:23:39 +0300

    https://online.sbis.ru/opendoc.html?guid=9990f9de-a7eb-4d33-974c-afe779fe31a2

--- Controls/_menu/Control.ts
+++ Controls/_menu/Control.ts
@@ -320,7 +320,7 @@ export default class MenuControl extends Control<IMenuControlOptions> implements
     }
 
     private _updateAfterLoad(items): void {
-        this._setButtonVisibleState(items, this._options);
+        this._updateItems(items, this._options);
     }
 
     private _getSelectionController(): SelectionController {
@@ -865,17 +865,20 @@ export default class MenuControl extends Control<IMenuControlOptions> implements
         };
         let listModel: Search<Model> | Collection<Model>;
 
-        if (options.searchParam && options.searchValue) {
+        const isSearchModel = options.searchParam && options.searchValue;
+        if (isSearchModel) {
             listModel = new Search({...collectionConfig,
                 nodeProperty: options.nodeProperty,
                 parentProperty: options.parentProperty,
-                root: options.root
+                root: options.root,
+                filter: options.allowPin ? MenuControl._searchHistoryDisplayFilter.bind(this) : null
             });
         } else {
+            const filterFunction = options.parentProperty && options.nodeProperty ? MenuControl._displayFilter.bind(this, options) : null;
             // В дереве не работает группировка,
             // ждем решения по ошибке https://online.sbis.ru/opendoc.html?guid=f4a3be79-5ec5-45d2-b742-2d585c5c069d
             listModel = new Collection({...collectionConfig,
-                filter: MenuControl._displayFilter.bind(this, options)
+                filter: filterFunction
             });
 
             if (options.groupProperty) {
@@ -891,7 +894,7 @@ export default class MenuControl extends Control<IMenuControlOptions> implements
 
         if (options.additionalProperty) {
             listModel.addFilter(this._additionalFilter);
-        } else if (options.allowPin && !options.subMenuLevel && !this._expander) {
+        } else if (!isSearchModel && options.allowPin && !options.subMenuLevel && !this._expander) {
             listModel.addFilter(this._limitHistoryFilter);
         }
         return listModel;
@@ -1022,11 +1025,11 @@ export default class MenuControl extends Control<IMenuControlOptions> implements
                     hasAdditional = item.get(options.additionalProperty) && !MenuControl._isHistoryItem(item);
                 }
             });
-        } else if (options.allowPin && !options.subMenuLevel) {
+        } else if (!(options.searchParam && options.searchValue) && options.allowPin && !options.subMenuLevel) {
             this._visibleIds = [];
             const fixedIds = [];
             factory(items).each((item) => {
-                if (options.searchParam && options.searchValue || MenuControl._isItemCurrentRoot(item, options))  {
+                if (MenuControl._isItemCurrentRoot(item, options))  {
                     if (item.get('doNotSaveToHistory')) {
                         fixedIds.push(item.getKey());
                     } else {
@@ -1039,8 +1042,6 @@ export default class MenuControl extends Control<IMenuControlOptions> implements
                 this._visibleIds.splice(options.maxHistoryVisibleItems);
             }
 
-            this._addParentIdForSearchMode(options, items);
-
             fixedIds.forEach((fixedId) => {
                 if (!this._visibleIds.includes(fixedId)) {
                     this._visibleIds.push(fixedId);
@@ -1048,18 +1049,6 @@ export default class MenuControl extends Control<IMenuControlOptions> implements
             });
         }
         return hasAdditional;
-    }
-
-    private _addParentIdForSearchMode(options, items): void {
-        if (options.searchParam && options.searchValue) {
-            const ids = this._visibleIds.slice();
-            ids.forEach((id) => {
-                const parent = items.getRecordById(id).get(options.parentProperty);
-                if (parent) {
-                    this._visibleIds.push(parent);
-                }
-            });
-        }
     }
 
     private _openSubMenuByKey(popupOptions?: IStickyPopupOptions, key?: string): void {
@@ -1338,7 +1327,7 @@ export default class MenuControl extends Control<IMenuControlOptions> implements
     private static _displayFilter(options: IMenuControlOptions, item: Model): boolean {
         let isVisible: boolean = true;
         const isStringType = typeof options.root === 'string';
-        if (item && item.get && options.parentProperty && options.nodeProperty) {
+        if (item && item.get) {
             let parent: TKey = item.get(options.parentProperty);
             if (parent === undefined) {
                 parent = null;
@@ -1352,6 +1341,13 @@ export default class MenuControl extends Control<IMenuControlOptions> implements
             isVisible = parent === options.root;
         }
         return isVisible;
+    }
+
+    private static _searchHistoryDisplayFilter(item: Model): boolean {
+        if (item && item.get) {
+            return !item.get('recent') && !item.get('frequent');
+        }
+        return true;
     }
 
     private static _isItemCurrentRoot(item: Model, options: IMenuControlOptions): boolean {
