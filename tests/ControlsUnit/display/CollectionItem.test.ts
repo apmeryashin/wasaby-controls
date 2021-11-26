c5012bb71fb07b3d14694e017af471b5fee9815a
pa.averkiev <pa.averkiev@tensor.ru>
2021-11-26 14:57:43 +0300

    https://online.sbis.ru/opendoc.html?guid=2ece146d-0a04-4c38-aaf8-7f63b8bbff1d делаем костылём. сли baseLine указано как none,то поведение по умолчанию. наче добавляем padding

--- tests/ControlsUnit/display/CollectionItem.test.ts
+++ tests/ControlsUnit/display/CollectionItem.test.ts
@@ -549,20 +549,20 @@ describe('Controls/_display/CollectionItem', () => {
         item.setMultiSelectVisibility('onhover');
         const onhoverMultiSelectClasses = item.getMultiSelectClasses('default');
         CssClassesAssert.isSame(onhoverMultiSelectClasses, requiredClasses +
-            ' controls-ListView__checkbox_marginTop_default controls-ListView__checkbox_position-default' +
+            ' controls-ListView__checkbox_marginTop_s controls-ListView__checkbox_position-default' +
             ' controls-ListView__checkbox-onhover');
 
         // multiselect onhover + selected
         item.setSelected(true, true);
         const selectedMultiSelectClasses = item.getMultiSelectClasses('default');
         CssClassesAssert.isSame(selectedMultiSelectClasses, requiredClasses +
-            ' controls-ListView__checkbox_marginTop_default controls-ListView__checkbox_position-default');
+            ' controls-ListView__checkbox_marginTop_s controls-ListView__checkbox_position-default');
 
         // custom position
         owner.getMultiSelectPosition = () => 'custom';
         const customMultiSelectClasses = item.getMultiSelectClasses('default');
         CssClassesAssert.isSame(customMultiSelectClasses, requiredClasses +
-            ' controls-ListView__checkbox_marginTop_default controls-ListView__checkbox_position-custom ');
+            ' controls-ListView__checkbox_marginTop_s controls-ListView__checkbox_position-custom ');
     });
 
     describe('.setEditing()', () => {
