d7b8d996438ef217f169f390780bfe06f51b3464
va.kolesov <va.kolesov@tensor.ru>
2021-11-26 19:43:15 +0300

    https://online.sbis.ru/opendoc.html?guid=8264d9c6-c6f5-4259-9f04-e515e41dc55e

---
+++ Controls-demo/list_new/ColumnsView/ItemsContainerPadding/Index.ts
@@ -0,0 +1,7 @@
+import { Control, TemplateFunction } from 'UI/Base';
+import template = require('wml!Controls-demo/list_new/ColumnsView/ItemsContainerPadding/Index');
+
+export default class RenderDemo extends Control {
+    protected _template: TemplateFunction = template;
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
