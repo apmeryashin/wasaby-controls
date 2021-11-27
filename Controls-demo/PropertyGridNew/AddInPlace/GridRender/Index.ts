7ab1184d2751aeb9964d14160da0ec6192dd0a2d
paaverkiev <pa.averkiev@corp.tensor.ru>
2021-11-27 18:48:11 +0300

    https://online.sbis.ru/opendoc.html?guid=0ac127f6-f2da-499b-ae46-b11418f6a0d3

---
+++ Controls-demo/PropertyGridNew/AddInPlace/GridRender/Index.ts
@@ -0,0 +1,62 @@
+import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
+import {IItemAction, TItemActionShowType} from 'Controls/itemActions';
+import {RecordSet} from 'Types/collection';
+import {Model} from 'Types/entity';
+import {default as IPropertyGridItem} from 'Controls/_propertyGrid/IProperty';
+
+import * as template from 'wml!Controls-demo/PropertyGridNew/AddInPlace/GridRender/GridRender';
+
+export default class Demo extends Control<IControlOptions> {
+    protected _template: TemplateFunction = template;
+    protected _editingObject: Model;
+    protected _typeDescription: RecordSet;
+    protected _itemActions: IItemAction[];
+
+    protected _beforeMount(): void {
+        this._editingObject = new Model<IPropertyGridItem>({
+            rawData: {
+                dynamicString: 'http://mysite.com'
+            }
+        });
+        this._typeDescription = new RecordSet<IPropertyGridItem>({
+            rawData: [
+                {
+                    caption: 'URL',
+                    name: 'dynamicString',
+                    editorOptions: {
+                        editable: true
+                    }
+                }
+            ],
+            keyProperty: 'name'
+        });
+        this._itemActions = [
+            {
+                id: 1,
+                icon: 'icon-Erase',
+                iconStyle: 'danger',
+                showType: TItemActionShowType.MENU,
+                title: 'Удалить',
+                handler: (item: Model) => {
+                    const key = item.getKey();
+                    this._typeDescription.remove(this._typeDescription.getRecordById(key));
+                }
+            }
+        ];
+    }
+
+    protected _beginAddEmpty(): void {
+        this._children.propertyGrid.beginAdd();
+    }
+
+    protected _beginAddTemplated(): void {
+        this._children.propertyGrid.beginAdd({
+            item: new Model({
+                keyProperty: 'key',
+                rawData: {key: ++this._fakeItemId, title: 'Новая запись...'}
+            })
+        });
+    }
+
+    static _styles: string[] = ['Controls-demo/PropertyGridNew/PropertyGrid'];
+}
