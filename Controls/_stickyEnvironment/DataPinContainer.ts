59244449fb23741290aeef15c09f5c3da839d34e
du.ufimcev <du.ufimcev@tensor.ru>
2021-11-29 10:55:04 +0500

    https://online.sbis.ru/opendoc.html?guid=40c691e3-62cc-4475-962a-0f183c5299b5

--- Controls/_stickyEnvironment/DataPinContainer.ts
+++ Controls/_stickyEnvironment/DataPinContainer.ts
@@ -12,16 +12,12 @@ import * as template from 'wml!Controls/_stickyEnvironment/DataPinContainer';
  */
 export interface IDataPinContainer extends IControlOptions {
     /**
-     * @cfg {any} Денные которые будут переданы в {@link Controls/stickyEnvironment:DataPinConsumer DataPinConsumer}
-     * при уходе текущего {@link Controls/stickyEnvironment:DataPinContainer DataPinContainer} за верхнюю границу
-     * {@link Controls/scroll:Container} (либо {@link Controls/scroll:IntersectionObserverController})
+     * @cfg {any} Данные которые будут переданы в {@link Controls/stickyEnvironment:DataPinConsumer DataPinConsumer} при уходе текущего {@link Controls/stickyEnvironment:DataPinContainer DataPinContainer} за верхнюю границу {@link Controls/scroll:Container} (либо {@link Controls/scroll:IntersectionObserverController})
      */
     data?: unknown;
 
     /**
-     * @cfg {String} Отступ от верхней границы {@link Controls/scroll:Container} (либо {@link Controls/scroll:IntersectionObserverController})
-     * при котором будет засчитано пересечение. Представляет собой строку с синтаксисом, аналогичным синтаксису свойства
-     * CSS margin-top.
+     * @cfg {String} Отступ от верхней границы {@link Controls/scroll:Container} (либо {@link Controls/scroll:IntersectionObserverController}) при котором будет засчитано пересечение. Представляет собой строку с синтаксисом, аналогичным синтаксису свойства CSS margin-top.
      *
      * @remark
      * Если пересечение должно быть засчитано раньше чем начинается верхняя граница, то значение должно быть отрицательным.
@@ -29,24 +25,40 @@ export interface IDataPinContainer extends IControlOptions {
      * @default '0px'
      */
     topMargin?: string;
+
+    /**
+     * @cfg {String} Отступ от нижней границы {@link Controls/scroll:Container} (либо {@link Controls/scroll:IntersectionObserverController}) при котором будет засчитано пересечение. Представляет собой строку с синтаксисом, аналогичным синтаксису свойства CSS margin-top.
+     *
+     * @default '0px'
+     */
+    bottomMargin?: string;
 }
 
 /**
- * Контрол контейнер на котором можно закрепить произвольные данные и который отслеживает свое положение внутри
- * {@link Controls/scroll:Container} (либо {@link Controls/scroll:IntersectionObserverController}).В случае ухода
- * данного контейнера за верхнюю границу <i>ScrollContainer</i> его данные будут переданы в {@link Controls/stickyEnvironment:DataPinConsumer DataPinConsumer}
- * в рамках родительского {@link Controls/stickyEnvironment:DataPinProvider DataPinProvider}.
+ * Контрол контейнер на котором можно закрепить произвольные данные и который отслеживает свое положение внутри {@link Controls/scroll:Container} (либо {@link Controls/scroll:IntersectionObserverController}).
+ * В случае ухода данного контейнера за верхнюю границу <i>ScrollContainer</i> его данные будут переданы в {@link Controls/stickyEnvironment:DataPinConsumer DataPinConsumer} в рамках родительского {@link Controls/stickyEnvironment:DataPinProvider DataPinProvider}.
+ *
+ * <strong>Важно</strong>
+ * {@link Controls/stickyEnvironment:DataPinContainer DataPinContainer} не должен лежать внутри элемента с <i>overflow: hidden</i>. Иначе это на нативном уровне ломает функционал IntersectionObserver.
+ * Единственный предок с <i>overflow: hidden</i> должен быть только {@link Controls/scroll:Container} (либо {@link Controls/scroll:IntersectionObserverController})
+ *
+ * @demo Controls-demo/StickyEnvironment/DataPinProvider/Base/Index
+ * @demo Controls-demo/StickyEnvironment/DataPinProvider/Grid/Index
+ * @demo Controls-demo/StickyEnvironment/DataPinProvider/Events/Index
+ *
+ * @implements Controls/stickyEnvironment:IDataPinContainer
  *
  * @public
  * @author Уфимцев Д.Ю.
  */
-export class DataPinContainer<T = unknown> extends Control<IControlOptions> {
+export class DataPinContainer extends Control<IDataPinContainer> {
+    protected _options: IDataPinContainer;
     protected _template: TemplateFunction = template;
 
     // значения пересечения при которых дергается _intersectHandler
     // при такой конфигурации событие будет стрелять трижды:
-    //  1. при пересечении блок с верхней границей контейнера
-    //  2. при уходе блока за верхнюю границу контейнера
+    //  1. при пересечении блока с верхней границей контейнера
+    //  2. при полном уходе блока за верхнюю границу контейнера
     //  3. при полном выходе блока ниже верхней границы контейнера
     protected _threshold: number[] = [0, 1];
 
@@ -55,28 +67,39 @@ export class DataPinContainer<T = unknown> extends Control<IControlOptions> {
 
     private pinController: PinController;
 
-    protected _intersectHandler(
-        event: SyntheticEvent,
-        entries: IntersectionObserverSyntheticEntry
-    ): void {
-        this.pinController.processIntersect(entries);
-    }
-
+    //region life circle hooks
     protected _beforeMount(options: IDataPinContainer, context: IPinConsumerContext): void {
         this.pinController = context.pinController;
-        this._updateRootMargin(options.topMargin);
+        this._updateRootMargin(options.topMargin, options.bottomMargin);
     }
 
     protected _beforeUpdate(newOptions: IDataPinContainer): void {
-        // tslint:disable-next-line:ban-ts-ignore
-        // @ts-ignore - не видит this._options
-        if (this._options.topMargin !== newOptions.topMargin) {
-            this._updateRootMargin(newOptions.topMargin);
+        if (
+            this._options.topMargin !== newOptions.topMargin ||
+            this._options.bottomMargin !== newOptions.bottomMargin
+        ) {
+            this._updateRootMargin(newOptions.topMargin, newOptions.bottomMargin);
         }
     }
 
-    private _updateRootMargin(top: string = '0px'): void {
-        this._rootMargin = `${top} 0px 0px 0px`;
+    protected _beforeUnmount(): void {
+        // При дестрое надо выкинуть наши данные из стека PinController'а
+        this.pinController.dropStackItem(this._options.data);
+    }
+    //endregion
+
+    /**
+     * Обработчик появления/скрытия в области видимости родительского IntersectionObserverController.
+     */
+    protected _intersectHandler(
+        event: SyntheticEvent,
+        entry: IntersectionObserverSyntheticEntry
+    ): void {
+        this.pinController.processIntersect(entry);
+    }
+
+    private _updateRootMargin(top: string = '0px', bottom: string = '0px'): void {
+        this._rootMargin = `${top} 0px ${bottom} 0px`;
     }
 
     static contextTypes(): object {
