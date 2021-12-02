b53a0ea2f5b5b48abd058e627a7118610ec6cb3a
ma.mochalov <ma.mochalov@tensor.ru>
2021-12-02 13:55:29 +0300

    https://online.sbis.ru/opendoc.html?guid=ee1cc0dd-034c-4463-9e66-a55c7861cf78

--- Controls-demo/EditableArea/ViewContent/Index.ts
+++ Controls-demo/EditableArea/ViewContent/Index.ts
@@ -1,5 +1,5 @@
 import {Record} from 'Types/entity';
-import {main as editObject, date as editObjectRange} from '../Data';
+import {main as editingObject, date as editingObjectRange} from '../Data';
 import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 // tslint:disable-next-line:ban-ts-ignore
 // @ts-ignore
@@ -7,8 +7,8 @@ import * as template from 'wml!Controls-demo/EditableArea/ViewContent/ViewConten
 
 class ViewContent extends Control<IControlOptions> {
     protected _template: TemplateFunction = template;
-    protected _editObject: Record = editObject;
-    protected _editObjectRange: Record = editObjectRange;
+    protected _editingObject: Record = editingObject;
+    protected _editingObjectRange: Record = editingObjectRange;
     static _styles: string[] = ['Controls-demo/Controls-demo'];
 }
 export default ViewContent;
