7384091f87c43e6ebadad3cec0418c7744b21e94
gd.kovalev <gd.kovalev@tensor.ru>
2021-12-02 12:33:34 +0700

    https://online.sbis.ru/opendoc.html?guid=c1640afb-d605-47dd-a2a4-d08f708a5f01

--- Controls/_validate/Validators/IsValidDateRange.ts
+++ Controls/_validate/Validators/IsValidDateRange.ts
@@ -10,9 +10,10 @@ const MAX_YEAR_VALUE =  new Date().getFullYear() + additionalYears;
 /**
  * Функция, позволяющая проверить период на валидность.
  * @class Controls/_validate/Validators/IsValidDateRange
- * @private
+ * @public
  * @author Красильников А.С.
  * @remark
+ * Валидатор вшит в контролы {@link Controls/date:BaseInput}, {@link Controls/date:Input}, {@link Controls/dateRange:BaseInput}
  * Подробнее о работе с валидацией читайте {@link /doc/platform/developmentapl/interface-development/forms-and-validation/validation/ здесь}.
  *
  * Аргументы функции:
