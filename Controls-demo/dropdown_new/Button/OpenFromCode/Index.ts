002b8e161d0cd71eeeda0a274289c60f54230571
Elina Zolotova <elinazolotov@gmail.com>
2021-12-08 12:26:50 +0300

    https://online.sbis.ru/opendoc.html?guid=9589587e-a90f-4375-95c5-e44ee2f65ce4

--- Controls-demo/dropdown_new/Button/OpenFromCode/Index.ts
+++ Controls-demo/dropdown_new/Button/OpenFromCode/Index.ts
@@ -9,36 +9,36 @@ export default class extends Control {
 
     protected _children: {
         button: Button
-    }
+    };
 
     protected _beforeMount(): void {
         this._source = new Memory({
             keyProperty: 'id',
             data: [
                 {
-                    id: 1,
+                    id: '1',
                     title: 'Task',
                     '@parent': true,
                     parent: null
                 },
-                { id: 2, title: 'Error in the development', '@parent': false, parent: null },
-                { id: 3, title: 'Commission', parent: 1 },
+                { id: '2', title: 'Error in the development', '@parent': false, parent: null },
+                { id: '3', title: 'Commission', parent: '1' },
                 {
-                    id: 4,
+                    id: '4',
                     title: 'Coordination',
-                    parent: 1,
+                    parent: '1',
                     '@parent': true
                 },
-                { id: 5, title: 'Application', parent: 1 },
-                { id: 6, title: 'Development', parent: 1, '@parent': true },
-                { id: 7, title: 'Exploitation', parent: 1 },
-                { id: 8, title: 'Coordination', parent: 4 },
-                { id: 9, title: 'Negotiate the discount', parent: 4 },
-                { id: 10, title: 'Coordination of change prices', parent: 4 },
-                { id: 11, title: 'Merge request', parent: 6 },
-                { id: 12, title: 'Changing the operation rule', parent: 6 },
-                { id: 13, title: 'Code review', parent: 6 },
-                { id: 14, title: 'Matching new dish', parent: 6 }
+                { id: '5', title: 'Application', parent: '1' },
+                { id: '6', title: 'Development', parent: '1', '@parent': true },
+                { id: '7', title: 'Exploitation', parent: '1' },
+                { id: '8', title: 'Coordination', parent: '4' },
+                { id: '9', title: 'Negotiate the discount', parent: '4' },
+                { id: '10', title: 'Coordination of change prices', parent: '4' },
+                { id: '11', title: 'Merge request', parent: '6' },
+                { id: '12', title: 'Changing the operation rule', parent: '6' },
+                { id: '13', title: 'Code review', parent: '6' },
+                { id: '14', title: 'Matching new dish', parent: '6' }
             ]
         });
     }
@@ -48,7 +48,7 @@ export default class extends Control {
             templateOptions: {
                 borderStyle: 'warning'
             }
-        }, 1);
+        }, '', '1');
     }
 
     _closeSubMenu(event, key): void {
