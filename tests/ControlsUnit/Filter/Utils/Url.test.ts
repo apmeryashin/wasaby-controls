bc1ce8c6b16333c4f260f0a76affeb50931aecf2
mk.siryakov <mk.siryakov@tensor.ru>
2021-12-15 13:44:58 +0300

    https://online.sbis.ru/opendoc.html?guid=328fb4b4-9183-4cc3-abb7-ddd6813537d7

--- tests/ControlsUnit/Filter/Utils/Url.test.ts
+++ tests/ControlsUnit/Filter/Utils/Url.test.ts
@@ -17,26 +17,42 @@ describe('Controls/_filter/Utils/Url', () => {
         const queryParams = getQueryParamsByFilter([
             {name: 'testName', value: 'testValue', textValue: 'testText', resetValue: null},
             {name: 'testName2', value: {value: 'testValue'}, textValue: 'testText', resetValue: null},
-            {name: 'testName', value: [{value: 'testValue'}], textValue: 'testText', resetValue: null}
+            {name: 'testName', value: [{value: 'testValue'}], textValue: 'testText', resetValue: null, visibility: true}
         ]);
-        const expected = {filter: '[{"name":"testName","value":"testValue","textValue":"testText"},{"name":"testName2","value":{"value":"testValue"},"textValue":"testText"},{"name":"testName","value":[{"value":"testValue"}],"textValue":"testText"}]'};
+        const expected = {filter: '[{"name":"testName","value":"testValue","textValue":"testText","visibility":' +
+                '{"$serialized$":"undef"}},{"name":"testName2","value":{"value":"testValue"},"textValue":"testText",' +
+                '"visibility":{"$serialized$":"undef"}},{"name":"testName","value":[{"value":"testValue"}],' +
+                '"textValue":"testText","visibility":true}]'};
 
         assert.deepEqual(queryParams, expected);
     });
 
     it('updateUrlByFilter', () => {
-        const items = [{name: 'testName', value: 'testValue', textValue: 'testText', resetValue: null}];
+        const items = [{
+            name: 'testName',
+            value: 'testValue',
+            textValue: 'testText',
+            resetValue: null,
+            visibility: true
+        }];
         const queryParams = getQueryParamsByFilter(items);
         const state = MaskResolver.calculateQueryHref(queryParams);
 
-        assert.deepEqual(queryParams, {filter: '[{"name":"testName","value":"testValue","textValue":"testText"}]'});
-        assert.deepEqual(state, '/?filter=%5B%7B%22name%22%3A%22testName%22%2C%22value%22%3A%22testValue%22%2C%22textValue%22%3A%22testText%22%7D%5D');
+        assert.deepEqual(queryParams, {filter: '[{"name":"testName","value":"testValue",' +
+                '"textValue":"testText","visibility":true}]'});
+        assert.deepEqual(state, '/?filter=%5B%7B%22name%22%3A%22testName%22%2C%22value%22%3A%22testValue' +
+            '%22%2C%22textValue%22%3A%22testText%22%2C%22visibility%22%3Atrue%7D%5D');
     });
 
     it('getFilterFromUrl', () => {
-        const urlFilter = {filter: '%5B%7B%22name%22%3A%22Organization%22%2C%22value%22%3A21391705%2C%22textValue%22%3A%22%D0%9D%D0%9E%D0%92%D0%AB%D0%99%20%D0%A2%D0%95%D0%A1%D0%A2%22%7D%5D'};
+        const urlFilter = {filter: '%5B%7B%22name%22%3A%22Organization%22%2C%22value%22%3A21391705%2C%22textValue' +
+                '%22%3A%22%D0%9D%D0%9E%D0%92%D0%AB%D0%99%20%D0%A2%D0%95%D0%A1%D0%A2%22%7D%5D'};
         sinon.stub(query, 'get').value(urlFilter);
 
-        assert.deepEqual(getFilterFromUrl(), [{name: 'Organization', value: 21391705, textValue: 'НОВЫЙ ТЕСТ'}]);
+        assert.deepEqual(getFilterFromUrl(), [{
+            name: 'Organization',
+            value: 21391705,
+            textValue: 'НОВЫЙ ТЕСТ'
+        }]);
     });
 });
