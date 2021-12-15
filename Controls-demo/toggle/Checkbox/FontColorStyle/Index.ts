4e363c38d2285305ea3b3242afaaf44a680a82e7
kraynovdo <do.kraynov@tensor.ru>
2021-12-15 07:37:55 +0300

    https://online.sbis.ru/opendoc.html?guid=899a7375-3710-461c-aa6c-bf73ab9a4e12 правка тестов

--- Controls-demo/toggle/Checkbox/FontColorStyle/Index.ts
+++ Controls-demo/toggle/Checkbox/FontColorStyle/Index.ts
@@ -1,5 +1,5 @@
 import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
-import controlTemplate = require('wml!Controls-demo/Toggle/Checkbox/FontColorStyle/FontStyles');
+import controlTemplate = require('wml!Controls-demo/toggle/Checkbox/FontColorStyle/FontStyles');
 
 class FontStyles extends Control<IControlOptions> {
     protected _template: TemplateFunction = controlTemplate;
