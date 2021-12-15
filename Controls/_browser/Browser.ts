a1ad8886a18fbf4c379e9ca105302727ddb4ad5b
am.gerasimov <am.gerasimov@tensor.ru>
2021-12-15 15:45:17 +0300

    https://online.saby.ru/opendoc.html?guid=f438614b-ed4b-41aa-8b86-2ddff7bda6c5

--- Controls/_browser/Browser.ts
+++ Controls/_browser/Browser.ts
@@ -998,7 +998,10 @@ export default class Browser extends Control<IBrowserOptions, TReceivedState> {
                 }
             });
         } else {
-            const filter = this._getSearchControllerSync().reset(true);
+            // Если Browser строится по sourceController'у со страницы, то и фильтр должен обновляться на странице
+            // в противном случае происходит рассинхрон фильтров в контексте и в контроллере
+            // Решается тут https://online.saby.ru/opendoc.html?guid=6c15ea37-fdf6-4dc8-a8fa-d54bd7be01bb
+            const filter = this._getSearchControllerSync().reset(!!this._options.sourceController);
             if (!isEqual(this._filter, filter)) {
                 this._filterChanged(null, filter);
             }
