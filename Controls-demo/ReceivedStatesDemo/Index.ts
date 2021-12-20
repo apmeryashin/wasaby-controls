f8d14febed430ff130fd1f797a4e0a0e1cdf7ac0
mi.hambelov <mi.hambelov@tensor.ru>
2021-12-20 19:48:31 +0300

    https://online.sbis.ru/opendoc.html?guid=79cdefe0-8605-4458-a39b-120730e6ed7a

--- Controls-demo/ReceivedStatesDemo/Index.ts
+++
@@ -1,6 +0,0 @@
-import { Control, TemplateFunction } from 'UI/Base';
-import * as template from 'wml!Controls-demo/ReceivedStatesDemo/Index';
-
-export default class extends Control {
-   protected _template: TemplateFunction = template;
-}
