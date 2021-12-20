8aa09fd7586264b39f68db063c8f8cc4472cba5f
mi.hambelov <mi.hambelov@tensor.ru>
2021-12-20 19:44:50 +0300

    https://online.sbis.ru/opendoc.html?guid=79cdefe0-8605-4458-a39b-120730e6ed7a

--- Controls-demo/SerializerDepsStorage/ModuleToRequire.js
+++
@@ -1,8 +0,0 @@
-define('Controls-demo/SerializerDepsStorage/ModuleToRequire', function() {
-   'use strict';
-   return {
-      myFunc: function() {
-         return 'function result';
-      }
-   };
-});
