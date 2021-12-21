28f28f3a3dc0f8264fb09237fcc2c82a2bb6996d
ma.mochalov <ma.mochalov@tensor.ru>
2021-12-21 16:00:09 +0300

    https://online.sbis.ru/opendoc.html?guid=617bc0fb-7db3-42c9-9580-5d65fa7086f2

--- Controls/jumpingLabel.ts
+++ Controls/jumpingLabel.ts
@@ -8,6 +8,6 @@
  * @author Крайнов Д.О.
  */
 
-export {default as Abstract} from './_jumpingLabel/Base';
+export {default as Abstract, IBaseOptions} from './_jumpingLabel/Base';
 export {default as InputContainer} from './_jumpingLabel/InputContainer';
 export {default as SelectionContainer} from './_jumpingLabel/SelectionContainer';
