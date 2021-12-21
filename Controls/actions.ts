4b28d9df69f2f7b725ded1ccd066fbe7650322dc
se.mihaylov <se.mihaylov@tensor.ru>
2021-12-21 19:05:12 +0300

    https://online.sbis.ru/opendoc.html?guid=b8a33f87-0e8a-45fd-925e-c1e2ad094511

--- Controls/actions.ts
+++ Controls/actions.ts
@@ -3,6 +3,8 @@
  * @library
  */
 
+import {IExecuteOptions} from './_actions/BaseAction';
+
 export {IAction} from './_actions/IAction';
 export {default as BaseAction, IBaseActionOptions as IActionOptions} from './_actions/BaseAction';
 export {default as Remove} from './_actions/MassActions/Remove';
@@ -12,3 +14,4 @@ export {default as MassAction} from './_actions/MassActions/MassAction';
 export {default as Sort} from './_actions/SortingActions/Sort';
 export {default as SortingMenuItemTemplate} from './_actions/SortingActions/MenuItemTemplate';
 export {default as Container} from 'Controls/_actions/Container';
+export {IExecuteOptions};
