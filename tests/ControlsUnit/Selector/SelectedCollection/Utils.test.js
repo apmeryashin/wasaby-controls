b79e99cd422b7c788e681e7b0fa779e8b6371d80
ea.melnikova <ea.melnikova@tensor.ru>
2021-12-23 12:40:57 +0300

    https://online.sbis.ru/opendoc.html?guid=caab9d45-1712-4cfc-b9bb-c7c701696f20

--- tests/ControlsUnit/Selector/SelectedCollection/Utils.test.js
+++ tests/ControlsUnit/Selector/SelectedCollection/Utils.test.js
@@ -1,7 +1,7 @@
 define(['Controls/_lookup/SelectedCollection/Utils'], function(Utils) {
    describe('Controls/_lookup/SelectedCollection/Utils', function() {
       it('getItemMaxWidth', function() {
-         assert.equal(Utils.getItemMaxWidth(0, 4, 1, 'oneRow', 20), 'calc(100% - 20px);');
+         assert.equal(Utils.getItemMaxWidth(0, 4, 1, 'oneRow', 20), undefined);
          assert.equal(Utils.getItemMaxWidth(0, 4, 2, 'oneRow', 20), undefined);
          assert.equal(Utils.getItemMaxWidth(0, 4, 2, 'default', 30), 'calc(100% - 30px);');
          assert.equal(Utils.getItemMaxWidth(1, 4, 2, 'default', 30), undefined);
