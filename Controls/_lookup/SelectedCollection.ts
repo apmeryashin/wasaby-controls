fdab7e560f2812751323d4a53957959bf83c4bf8
ea.melnikova <ea.melnikova@tensor.ru>
2021-12-23 15:08:54 +0300

    https://online.sbis.ru/opendoc.html?guid=6c5d5227-1cfc-43e4-b6cb-8f86f48824a7

--- Controls/_lookup/SelectedCollection.ts
+++ Controls/_lookup/SelectedCollection.ts
@@ -171,14 +171,14 @@ class SelectedCollection extends Control<ISelectedCollectionOptions, number> {
                                readOnly,
                                itemsLayout,
                                fonSize,
-                               multiline
+                               multiLine
                             }: ISelectedCollectionOptions): number {
       // in mode read only and single line, counter does not affect the collection
       if (readOnly && itemsLayout === 'oneRow') {
          return 0;
       }
 
-      return selectedCollectionUtils.getCounterWidth(itemsCount, this._options.theme, fonSize, multiline);
+      return selectedCollectionUtils.getCounterWidth(itemsCount, this._options.theme, fonSize, multiLine);
    }
 
    private _isShowCounter(itemsCount: number, maxVisibleItems: number): boolean {
