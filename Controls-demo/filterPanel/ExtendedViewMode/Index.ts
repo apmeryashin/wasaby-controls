778a8112cd8a98115a73586be17285514c6c5292
ea.melnikova <ea.melnikova@tensor.ru>
2021-12-23 14:34:42 +0300

    https://online.sbis.ru/opendoc.html?guid=6c5d5227-1cfc-43e4-b6cb-8f86f48824a7

---
+++ Controls-demo/filterPanel/ExtendedViewMode/Index.ts
@@ -0,0 +1,55 @@
+import {Control, TemplateFunction} from 'UI/Base';
+import * as Template from 'wml!Controls-demo/filterPanel/ExtendedViewMode/Index';
+import {Memory} from 'Types/source';
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+    protected _filterSource: object[] = [];
+    protected _viewSource: Memory = null;
+
+    protected _beforeMount(): void {
+        this._viewSource = new Memory({
+            data: [
+                {department: 'Разработка', title: 'Разработка'},
+                {department: 'Продвижение СБИС', title: 'Продвижение СБИС'},
+                {department: 'Федеральная клиентская служка', title: 'Федеральная клиентская служка'}
+            ],
+            keyProperty: 'department',
+            filter: (item, queryFilter) => {
+                let addToData = true;
+                for (const filterField in queryFilter) {
+                    if (queryFilter.hasOwnProperty(filterField) && item.get(filterField)) {
+                        const filterValue = queryFilter[filterField];
+                        const itemValue = item.get(filterField);
+                        addToData = filterValue.includes(itemValue) || !filterValue.length;
+                    }
+                }
+                return addToData;
+            }
+        });
+        this._filterSource = [
+            {
+                name: 'department',
+                value: [],
+                resetValue: [],
+                editorTemplateName: 'Controls/filterPanel:DropdownEditor',
+                editorOptions: {
+                    source: new Memory({
+                        keyProperty: 'department',
+                        data:  [
+                            {department: 'Разработка', title: 'Разработка'},
+                            {department: 'Продвижение СБИС', title: 'Продвижение СБИС'},
+                            {department: 'Федеральная клиентская служба', title: 'Федеральная клиентская служба'}
+                        ]
+                    }),
+                    displayProperty: 'title',
+                    keyProperty: 'department',
+                    extendedCaption: 'Непрочитанные'
+                },
+                viewMode: 'extended'
+            }
+        ];
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo', 'Controls-demo/filterPanel/Index'];
+}
