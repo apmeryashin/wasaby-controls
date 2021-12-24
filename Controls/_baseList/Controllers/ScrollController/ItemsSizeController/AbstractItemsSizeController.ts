92ab7b7a381dd66d4e76ef97892c72ed6580dd04
ka.panihin <ka.panihin@tensor.ru>
2021-12-24 10:06:33 +0300

    https://online.sbis.ru/opendoc.html?guid=5a8bb774-53fc-44d5-a2ed-c4ea67a696ce

--- Controls/_baseList/Controllers/ScrollController/ItemsSizeController/AbstractItemsSizeController.ts
+++ Controls/_baseList/Controllers/ScrollController/ItemsSizeController/AbstractItemsSizeController.ts
@@ -95,41 +95,15 @@ export abstract class AbstractItemsSizesController {
                 );
             } else {
                 let position = itemsRange.startIndex;
-
-                // Нужно учитывать оффсет элементов скрытых виртуальным диапазоном.
-                // Например, был диапазон 0 - 10, стал 5 - 15
-                // У 5-ой записи offset === 0, но перед ней есть еще 5-ть скрытых записей, у которых мы знаем offset.
-                // offset после последней скрытой записи равен ее offset + size.
-                let hiddenItemsOffset = 0;
-                if (position > 0) {
-                    const firstItemOffset = this._itemsSizes[0].offset;
-                    const lastHiddenItem = this._itemsSizes[position - 1];
-                    // нужно вычитать оффсет первой записи, чтобы он не учитывался дважды, когда мы будем прибавлять
-                    // hiddenItemsOffset к элементам нового диапазона.
-                    // Иначе будет дважды посчитана высота ромашки или хедера.
-                    hiddenItemsOffset = lastHiddenItem.offset + lastHiddenItem.size - firstItemOffset;
-                }
                 Array.from(itemsElements).forEach((element: HTMLElement) => {
                     const prevItemSize = this._itemsSizes[position - 1];
                     // оффсет не учитывает margin-ы, нужно будет решить эту проблему. offsetTop ее не решает.
+                    // Если брать offsetTop у записи, то возникает еще проблема с застикаными записями.
                     const offset = prevItemSize ? prevItemSize.offset + prevItemSize.size : 0;
-                    this._itemsSizes[position] = {
-                        size: this._getItemSize(element),
-                        offset: offset + hiddenItemsOffset
-                    };
+                    const size = this._getItemSize(element);
+                    this._itemsSizes[position] = { size, offset };
                     position++;
                 });
-
-                // Т.к. мы обновили размеры в начале массива, то у последующих элементов нужно обновить offset
-                if (itemsRange.endIndex < this._itemsSizes.length) {
-                    const lastUpdatedItem = this._itemsSizes[itemsRange.endIndex - 1];
-                    const firstNotUpdatedItem = this._itemsSizes[itemsRange.endIndex];
-                    const updatedItemsOffset = lastUpdatedItem.offset + lastUpdatedItem.size
-                        - firstNotUpdatedItem.offset;
-                    for (let i = itemsRange.endIndex; i < this._itemsSizes.length; i++) {
-                        this._itemsSizes[i].offset += updatedItemsOffset;
-                    }
-                }
             }
         } else {
             for (let position = itemsRange.startIndex; position <= itemsRange.endIndex; position++) {
