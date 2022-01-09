bb0b0dc181112ebbfa8bd36fb4f780a0fee2755f
as.but <as.but@tensor.ru>
2022-01-09 13:52:53 +0500

    https://online.sbis.ru/opendoc.html?guid=c49c4b80-a7e6-4a3f-be56-42e3755c6d18

--- Controls/_newBrowser/View/View.ts
+++ Controls/_newBrowser/View/View.ts
@@ -371,15 +371,22 @@ export default class View extends Control<IOptions, IReceivedState> {
         return {...listConfig, ...listOptions};
     }
 
-    protected _onExplorerItemClick(
+    protected _onMasterItemClick(
         event: SyntheticEvent,
-        isMaster: boolean,
+        item: Model,
+        clickEvent: unknown,
+        columnIndex?: number
+    ) {
+        return this._notify('itemClick', [item, clickEvent, columnIndex, 'master']);
+    }
+
+    protected _onDetailItemClick(
+        event: SyntheticEvent,
         item: Model,
         clickEvent: unknown,
         columnIndex?: number
     ): unknown {
-        const listId = isMaster ? 'master' : 'detail';
-        return this._notify('itemClick', [item, clickEvent, columnIndex, listId]);
+        return this._notify('itemClick', [item, clickEvent, columnIndex, 'detail']);
     }
 
     protected _createTemplateControllers(cfg: IBrowserViewConfig, options: IOptions): void {
