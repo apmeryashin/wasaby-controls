b0098ede064957b6eef0d27b06c8491d3d86f8b7
pa.averkiev <pa.averkiev@tensor.ru>
2022-01-12 17:33:25 +0300

    https://online.sbis.ru/opendoc.html?guid=dd8062bb-8f6e-437d-b820-dd8d23185df7

--- Controls/_baseList/BaseControl.ts
+++ Controls/_baseList/BaseControl.ts
@@ -3409,15 +3409,11 @@ export default class BaseControl<TOptions extends IBaseControlOptions = IBaseCon
      * Замораживает hover подсветку строки для указанной записи
      */
     freezeHoveredItem(item: Model): void {
-        // Если используется этот метод, значит прикладник самостоятельно запоминает item
-        // при этом метод может вызваться отложенно уже ПОСЛЕ того,
-        // как новые данные загружены и готова модель.
-        // Смотреть на состояние загрузки в этом случае не имеет смысла.
-        const collectionItem = item && this._listViewModel.getItemBySourceKey(item.getKey());
-        if (collectionItem) {
-            _private.freezeHoveredItem(this, collectionItem);
+        const collectionItem = this._listViewModel.getItemBySourceItem(item);
+        if (!collectionItem) {
+            Logger.error('freezeHoveredItem(). Указанный item отсутствует в коллекции.', this);
         } else {
-            Logger.warning('freezeHoveredItem(). Указанный item отсутствует в коллекции.', this);
+            _private.freezeHoveredItem(this, collectionItem);
         }
     }
 
