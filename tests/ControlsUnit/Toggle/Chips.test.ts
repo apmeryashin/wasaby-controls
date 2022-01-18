967c44a06feaf88a0a46422aebed315f4ac23bea
ma.mochalov <ma.mochalov@tensor.ru>
2022-01-18 10:50:23 +0300

    https://online.sbis.ru/opendoc.html?guid=e0d7c407-727d-4bae-847a-d5f659dbd6a8

--- tests/ControlsUnit/Toggle/Chips.test.ts
+++ tests/ControlsUnit/Toggle/Chips.test.ts
@@ -1,61 +1,45 @@
 import {Chips} from 'Controls/toggle';
 import {RecordSet} from 'Types/collection';
-import {Model} from 'Types/entity';
 import {assert} from 'chai';
 
 describe('Controls/toggle:Chips', () => {
     it('Chips _onItemClick', () => {
         const control = new Chips({});
-        const item1 = new Model({rawData: {id: '1', caption: 'Название 1'}, keyProperty: 'id'});
-        const item2 = new Model({rawData: {id: '2', caption: 'Название 2'}, keyProperty: 'id'});
-        const item3 = new Model({rawData: {id: '3', caption: 'Название 3'}, keyProperty: 'id'});
+        const recordSet = new RecordSet({
+            rawData: [
+                {
+                    id: '1',
+                    caption: 'Название 1'
+                },
+                {
+                    id: '2',
+                    caption: 'Название 2'
+                },
+                {
+                    id: '3',
+                    caption: 'Название 3'
+                }
+            ],
+            keyProperty: 'id'
+        });
         control.saveOptions({
             keyProperty: 'id',
             selectedKeys: ['2'],
-            items: new RecordSet({
-                rawData: [
-                    {
-                        id: '1',
-                        caption: 'Название 1'
-                    },
-                    {
-                        id: '2',
-                        caption: 'Название 2'
-                    },
-                    {
-                        id: '3',
-                        caption: 'Название 3'
-                    }
-                ],
-                keyProperty: 'id'
-            })
+            items: recordSet
         });
-        let step: number = 1;
         const sandbox = sinon.createSandbox();
-        sandbox.replace(control, '_notify', ((name, args: string[]) => {
-            switch (step) {
-                case 1:
-                    assert.equal(args[1][0], '1');
-                    break;
-                case 2:
-                    assert.equal(args[1][0], '3');
-                    break;
-                case 3:
-                    assert.equal(args[2][0], '3');
-                    break;
-                case 4:
-                    assert.equal(args[2][0], '2');
-                    break;
-            }
-            control._options.selectedKeys = args[0];
-        }));
-        control._onItemClick({}, item1);
-        step++;
-        control._onItemClick({}, item3);
-        step++;
-        control._onItemClick({}, item3);
-        step++;
-        control._onItemClick({}, item2);
+        const notifySpy = sandbox.spy(control, '_notify');
+        control._onItemClick({}, recordSet.at(0));
+        assert.equal(notifySpy.args[0][1][1][0], '1');
+        control._options.selectedKeys = notifySpy.args[0][1][0];
+        control._onItemClick({}, recordSet.at(2));
+        assert.equal(notifySpy.args[1][1][1][0], '3');
+        control._options.selectedKeys = notifySpy.args[1][1][0];
+        control._onItemClick({}, recordSet.at(2));
+        assert.equal(notifySpy.args[2][1][2][0], '3');
+        control._options.selectedKeys = notifySpy.args[2][1][0];
+        control._onItemClick({}, recordSet.at(1));
+        assert.equal(notifySpy.args[3][1][2][0], '2');
         sandbox.restore();
     });
 });
