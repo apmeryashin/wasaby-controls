f926a03f23e1521b20587b4cef9f871373c873f9
ea.melnikova <ea.melnikova@tensor.ru>
2022-01-19 12:54:29 +0300

    https://online.sbis.ru/opendoc.html?guid=bcbf439b-88a2-44e4-8a8c-d97bb8f8f6df

---
+++ Controls/_filterPanel/Editors/Date.ts
@@ -0,0 +1,82 @@
+import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
+import {SyntheticEvent} from 'Vdom/Vdom';
+import * as DateTemplate from 'wml!Controls/_filterPanel/Editors/Date';
+import {StringValueConverter} from 'Controls/date';
+import 'css!Controls/filterPanel';
+import {Range} from 'Controls/dateUtils';
+
+interface IDateOptions extends IControlOptions {
+    propertyValue: Date;
+    mask?: string;
+}
+
+/**
+ * Контрол используют в качестве редактора для поля ввода даты.
+ * @class Controls/_filterPanel/Editors/Date
+ * @extends UI/Base:Control
+ * @mixes Controls/date:Input
+ * @author Мельникова Е.А.
+ * @demo Controls-demo/filterPanelPopup/Editors/Date/Index
+ * @public
+ */
+
+/**
+ * @name Controls/_filterPanel/Editors/Date#additionalText
+ * @cfg {string} Текст, который будет отображен слева от поля ввода даты.
+ */
+
+class DateEditor extends Control<IDateOptions> {
+    protected _template: TemplateFunction = DateTemplate;
+    protected _value: Date = null;
+    protected _stringValueConverter: StringValueConverter = null;
+
+    protected _beforeMount(options: IDateOptions): void {
+        this._value = options.propertyValue;
+    }
+
+    protected _beforeUpdate(newOptions: IDateOptions): void {
+        if (newOptions.propertyValue !== this._options.propertyValue) {
+            this._value = newOptions.propertyValue;
+        }
+    }
+
+    protected _valueChangedHandler(event: SyntheticEvent, newValue: Date): void {
+        this._propertyValueChanged(newValue);
+    }
+
+    protected _extendedCaptionClickHandler(): void {
+        this._propertyValueChanged(null);
+    }
+
+    protected _propertyValueChanged(newValue: Date): void {
+        const extendedValue = {
+            value: newValue,
+            textValue: this._getTextValue(newValue),
+            viewMode: 'basic'
+        };
+        this._notify('propertyValueChanged', [extendedValue], {bubbling: true});
+    }
+
+    protected _getTextValue(value: Date): string {
+        if (!this._stringValueConverter) {
+            this._stringValueConverter = new StringValueConverter();
+        }
+        return this._stringValueConverter.getStringByValue(value, this._options.mask);
+    }
+
+    static getDefaultOptions(): object {
+        return {
+            mask: Range.dateMaskConstants.DD_MM_YY
+        };
+    }
+}
+
+Object.defineProperty(DateEditor, 'defaultProps', {
+    enumerable: true,
+    configurable: true,
+
+    get(): object {
+        return DateEditor.getDefaultOptions();
+    }
+});
+export default DateEditor;
---
+++ Controls/_filterPanel/Editors/Input.ts
@@ -0,0 +1,56 @@
+import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
+import {SyntheticEvent} from 'Vdom/Vdom';
+import * as InputTemplate from 'wml!Controls/_filterPanel/Editors/Input';
+import 'css!Controls/filterPanel';
+
+interface IInputOptions extends IControlOptions {
+    propertyValue: string;
+}
+
+/**
+ * Контрол используют в качестве редактора поля ввода.
+ * @class Controls/_filterPanel/Editors/Input
+ * @extends UI/Base:Control
+ * @mixes Controls/input:Text
+ * @author Мельникова Е.А.
+ * @demo Controls-demo/filterPanelPopup/Editors/Input/Index
+ * @public
+ */
+
+/**
+ * @name Controls/_filterPanel/Editors/Input#additionalText
+ * @cfg {string} Текст, который будет отображен слева от поля ввода.
+ */
+
+class InputEditor extends Control<IInputOptions> {
+    protected _template: TemplateFunction = InputTemplate;
+    protected _value: string = null;
+
+    protected _beforeMount(options: IInputOptions): void {
+        this._value = options.propertyValue;
+    }
+
+    protected _beforeUpdate(newOptions: IInputOptions): void {
+        if (newOptions.propertyValue !== this._options.propertyValue) {
+            this._value = newOptions.propertyValue;
+        }
+    }
+
+    protected _valueChangedHandler(event: SyntheticEvent, newValue: string): void {
+        this._propertyValueChanged(newValue);
+    }
+
+    protected _extendedCaptionClickHandler(): void {
+        this._propertyValueChanged(null);
+    }
+
+    protected _propertyValueChanged(newValue: string): void {
+        const extendedValue = {
+            value: newValue,
+            textValue: newValue,
+            viewMode: 'basic'
+        };
+        this._notify('propertyValueChanged', [extendedValue], {bubbling: true});
+    }
+}
+export default InputEditor;
