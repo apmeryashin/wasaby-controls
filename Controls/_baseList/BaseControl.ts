9543fdcb1ababf6f61b398d4afc26cd3bfb29836
va.kolesov <va.kolesov@tensor.ru>
2022-01-19 15:33:19 +0300

    https://online.sbis.ru/opendoc.html?guid=b75fc04e-8534-4dc8-b2fe-e439db609ffd

--- Controls/_baseList/BaseControl.ts
+++ Controls/_baseList/BaseControl.ts
@@ -3429,7 +3429,7 @@ export default class BaseControl<TOptions extends IBaseControlOptions = IBaseCon
             additionalTriggersOffsets: this._getAdditionalTriggersOffsets(),
             totalCount: this._listViewModel.getCount(),
 
-            feature1183225611: options.virtualScrollConfig.feature1183225611,
+            feature1183225611: options.virtualScrollConfig?.feature1183225611,
 
             scrollToElementUtil: (container, position, force): Promise<void> => {
                 this._scrollControllerInitializeChangeScroll = true;
