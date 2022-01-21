d21e4b88ab409ea033a42887aeea1db65dd26505
as.avramenko <as.avramenko@tensor.ru>
2022-01-21 15:18:21 +0300

    part 1 - join strategies https://online.sbis.ru/opendoc.html?guid=d26eb342-c3ff-4892-84af-27819a43196a

--- Controls/_treeGrid/display/itemsStrategy/NodeFooter.ts
+++ Controls/_treeGrid/display/itemsStrategy/NodeFooter.ts
@@ -1,7 +1,9 @@
-import {itemsStrategy} from 'Controls/display';
+import { Model } from 'Types/entity';
+import { itemsStrategy, TreeItem } from 'Controls/display';
+import {ISortOptions} from 'Controls/_display/itemsStrategy/NodeFooter';
 
 export default class TreeGridNodeFooter extends itemsStrategy.NodeFooter {
-    protected _hasNodeFooterViewConfig(options): boolean {
-        return super._hasNodeFooterViewConfig(options) || options.display.hasNodeFooterColumns();
+    protected _shouldAddExtraItem(item: TreeItem<Model>, options: ISortOptions): boolean {
+        return super._shouldAddExtraItem(item, options) || options.display.hasNodeFooterColumns();
     }
 }
