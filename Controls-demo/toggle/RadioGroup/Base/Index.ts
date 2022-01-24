a88f00e2f4f5d266406aca4d159964f9407e4fc9
in.kondratev <in.kondratev@tensor.ru>
2022-01-24 11:25:10 +0300

    https://online.sbis.ru/opendoc.html?guid=1c1027cc-d7f7-4408-a798-4443029e61a8

--- Controls-demo/toggle/RadioGroup/Base/Index.ts
+++ Controls-demo/toggle/RadioGroup/Base/Index.ts
@@ -1,15 +1,33 @@
 import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 import {Memory} from 'Types/source';
+import {RecordSet} from 'Types/collection';
 import Template = require('wml!Controls-demo/toggle/RadioGroup/Base/Template');
 
 class Base extends Control<IControlOptions> {
     protected _template: TemplateFunction = Template;
     protected _selectedKey: string = '1';
     protected _selectedKey2: string = '1';
+    protected _selectedKey3: string = '1';
     protected _source: Memory;
+    protected _items: RecordSet;
     protected _displayProperty: string = 'caption';
 
     protected _beforeMount(): void {
+        this._items = new RecordSet({
+            keyProperty: 'id',
+            rawData: [
+                {
+                    id: '1',
+                    title: 'item-1'
+                }, {
+                    id: '2',
+                    title: 'item-2'
+                }, {
+                    id: '3',
+                    title: 'item-3'
+                }
+            ]
+        });
         this._source = new Memory({
             keyProperty: 'id',
             displayProperty: 'caption',
