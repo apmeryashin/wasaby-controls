544e72af9202d138c010368790902f836817e267
pa.averkiev <pa.averkiev@tensor.ru>
2022-01-25 15:41:01 +0300

    https://online.sbis.ru/opendoc.html?guid=af75ec96-be70-4353-b2fd-4f378b8bfc14

--- Controls-demo/list_new/ColumnsView/MasterDetail/Index.ts
+++ Controls-demo/list_new/ColumnsView/MasterDetail/Index.ts
@@ -5,7 +5,6 @@ import * as data from 'Controls-demo/DragNDrop/MasterDetail/Data';
 import cInstance = require('Core/core-instance');
 import {Memory, CrudEntityKey} from 'Types/source';
 import {ItemsEntity, ListItems} from 'Controls/dragnDrop';
-import * as TaskEntity from 'Controls-demo/DragNDrop/MasterDetail/TasksEntity';
 import { TItemsReadyCallback } from 'Controls-demo/types';
 import {RecordSet} from 'Types/collection';
 import {SyntheticEvent} from 'Vdom/Vdom';
@@ -28,42 +27,46 @@ export default class RenderDemo extends Control {
     protected _itemsReadyCallbackDetail: TItemsReadyCallback;
     protected _itemsMaster: RecordSet;
     protected _itemsDetail: RecordSet;
+    private _detailRoot: CrudEntityKey;
 
     protected _dataArray: Array<{key: number, title: string, description: string}>;
 
-    _initSource(): void {
-        this._detailSource = new Memory({
-            keyProperty: 'id',
-            data: data.detail
-        });
-
+    protected _beforeMount(): void {
+        this._detailRoot = '0';
         this._masterSource = new Memory({
             keyProperty: 'id',
             data: data.master
         });
-    }
-
-    protected _beforeMount(): void {
-        this._initSource();
+        this._detailSource = new Memory({
+            keyProperty: 'id',
+            data: data.detail,
+            filter: (item, queryFilter) => {
+                const parent = item.get('parent');
+                return parent === this._detailRoot;
+            }
+        });
         this._itemsReadyCallbackMaster = this._itemsReadyMaster.bind(this);
         this._itemsReadyCallbackDetail = this._itemsReadyDetail.bind(this);
     }
-    _afterMount(): void {
-        this._initSource();
+
+    protected _masterItemClick(event: SyntheticEvent, item: Model): void {
+        this._detailRoot = item.getKey();
+        this._children.detailList.reload();
     }
-    _itemsReadyMaster(items: RecordSet): void {
+
+    protected _itemsReadyMaster(items: RecordSet): void {
         this._itemsMaster = items;
     }
 
-    _itemsReadyDetail(items: RecordSet): void {
+    protected _itemsReadyDetail(items: RecordSet): void {
         this._itemsDetail = items;
     }
 
-    _dragEnterMaster(_: SyntheticEvent, entity: ItemsEntity): boolean {
-        return cInstance.instanceOfModule(entity, 'Controls-demo/DragNDrop/MasterDetail/TasksEntity');
+    protected _dragEnterMaster(_: SyntheticEvent, entity: ItemsEntity): boolean {
+        return cInstance.instanceOfModule(entity, 'Controls/dragnDrop:ListItems');
     }
 
-    _dragStartMaster(_: SyntheticEvent, items: RecordSet): ListItems {
+    protected _dragStartMaster(_: SyntheticEvent, items: RecordSet): ListItems {
         const firstItem = this._itemsMaster.getRecordById(items[0]);
         return new ListItems({
             items,
@@ -71,22 +74,24 @@ export default class RenderDemo extends Control {
         });
     }
 
-    _dragStartDetail(_: SyntheticEvent, items: CrudEntityKey[]): TaskEntity {
+    protected _dragStartDetail(_: SyntheticEvent, items: CrudEntityKey[]): ListItems {
         const firstItem = this._itemsDetail.getRecordById(items[0]);
-        return new TaskEntity({
+        return new ListItems({
             items,
             mainText: firstItem.get('name'),
             image: firstItem.get('img'),
-            additionalText: firstItem.get('shortMsg')
+            additionalText: firstItem.get('shortMsg'),
+            detail: true
         });
     }
 
-    _dragEndMaster(_: SyntheticEvent, entity: ItemsEntity, target: Model, position: string): void {
+    protected _dragEndMaster(_: SyntheticEvent, entity: ItemsEntity, target: Model, position: string): void {
         let targetId = null;
         const items = entity.getItems();
 
-        if (cInstance.instanceOfModule(entity, 'Controls-demo/DragNDrop/MasterDetail/TasksEntity')) {
-            targetId = target.get('key');
+        // Пернос из правого реестра
+        if (entity.getOptions().detail) {
+            targetId = target.getKey();
             items.forEach((key: string | number): void => {
                 const item = this._itemsDetail.getRecordById(key);
                 item.set('parent', targetId);
@@ -105,7 +110,7 @@ export default class RenderDemo extends Control {
         }
     }
 
-    _dragEndDetail(_: SyntheticEvent, entity: ItemsEntity, target: Model, position: string): void {
+    protected _dragEndDetail(_: SyntheticEvent, entity: ItemsEntity, target: Model, position: string): void {
         const selection: ISelectionObject = {
             selected: entity.getItems(),
             excluded: []
