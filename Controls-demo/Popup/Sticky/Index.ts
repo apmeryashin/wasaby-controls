7ab8a5f6b624b72765473a269beb20797c0ed30a
gd.kovalev <gd.kovalev@tensor.ru>
2022-02-03 10:01:09 +0700

    https://online.sbis.ru/opendoc.html?guid=2cc0cd8e-75dd-40e9-91e5-449427046e83

--- Controls-demo/Popup/Sticky/Index.ts
+++ Controls-demo/Popup/Sticky/Index.ts
@@ -6,19 +6,10 @@ import 'wml!Controls-demo/Popup/Opener/DialogTpl';
 class PopupStack extends Control<IControlOptions> {
     protected _template: TemplateFunction = template;
 
-    protected openStickyScroll(): void {
+    protected openSticky(): void {
         this._children.sticky.open({
             target: this._children.stickyButton._container,
             opener: this._children.stickyButton,
-            actionOnScroll: 'close',
-            template: 'wml!Controls-demo/Popup/Opener/DialogTpl'
-        });
-    }
-    protected openSticky(): void {
-        this._children.sticky.open({
-            target: this._children.stickyButton2._container,
-            opener: this._children.stickyButton2,
-            actionOnScroll: 'track',
             template: 'wml!Controls-demo/Popup/Opener/DialogTpl'
         });
     }
