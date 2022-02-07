bc6d4251a849fb505252098ae6dbacdcfb1ae15c
do.kraynov <do.kraynov@tensor.ru>
2022-02-07 12:39:21 +0300

    test push

--- Controls/viewCommands.ts
+++ Controls/viewCommands.ts
@@ -4,3 +4,4 @@ export {default as AtomicRemove} from 'Controls/_viewCommands/AtomicRemove';
 export {default as Reload} from 'Controls/_viewCommands/Reload';
 export {default as PartialReload} from 'Controls/_viewCommands/PartialReload';
 export {default as Move} from 'Controls/_viewCommands/Move';
+
